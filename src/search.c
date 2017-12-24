e5ac6ce7bf71b7457d1baa663f4dd815b7ff529d
AndyGrant <Andrew@grantnet.us>
2017-12-24 02:34:29 -0500

    .

--- src/search.c
+++ src/search.c
@@ -414,7 +414,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             
         int rbeta = MIN(beta + 100, MATE - MAX_HEIGHT - 1);
             
-        initializeMovePicker(&movePicker, thread, NONE_MOVE, height, 1);
+        initializeMovePicker(&movePicker, thread, NONE_MOVE, height, 0);
         
         while ((currentMove = selectNextMove(&movePicker, board)) != NONE_MOVE){
             
