eab402ac3570a50cb7a058e018295f08f15a1176
AndyGrant <Andrew@grantnet.us>
2018-01-01 18:11:46 -0500

    .

--- src/search.h
+++ src/search.h
@@ -56,7 +56,7 @@ int valueToTT(int value, int height);
 
 static const int RazorDepth = 4;
 
-static const int RazorMargins[] = {0, 450, 480, 520, 580};
+static const int RazorMargins[] = {0, 250, 280, 320, 380};
 
 static const int BetaPruningDepth = 8;
 
