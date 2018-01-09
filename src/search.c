388a019e9088ee57acbc7878a413539b5c22c1f1
AndyGrant <Andrew@grantnet.us>
2018-01-08 19:38:28 -0500

    .

--- src/search.c
+++ src/search.c
@@ -65,10 +65,12 @@ uint16_t getBestMove(Thread* threads, Board* board, Limits* limits, double time,
         info.maxusage   = mtg >= 0 ? 2.8 * (time / (mtg + 1)) : 4.5 * (time / 25);
         info.idealusage = MIN(info.idealusage, time - 20);
         info.maxusage   = MIN(info.maxusage,   time - 20);
+        info.baseusage  = info.idealusage;
     }
     
     // UCI command told us to look for exactly X seconds
     if (limits->limitedByTime){
+        info.baseusage  = limits->timeLimit;
         info.idealusage = limits->timeLimit;
         info.maxusage   = limits->timeLimit;
     }
@@ -157,12 +159,12 @@ void* iterativeDeepening(void* vthread){
         if (limits->limitedBySelf){
             
             // Increase our time if the score suddently dropped by eight centipawns
-            if (depth >= 4 && info->values[depth - 1] > value + 8)
-                info->idealusage = MIN(info->maxusage, info->idealusage * 1.10);
+            if (depth >= 8 && info->values[depth-1] > value + 8)
+                info->idealusage = MIN(info->maxusage, info->idealusage + info->baseusage * 1.10);
             
             // Increase our time if the pv has changed across the last two iterations
-            if (depth >= 4 && info->bestmoves[depth - 1] != thread->pv.line[0])
-                info->idealusage = MIN(info->maxusage, info->idealusage * 1.35);
+            if (depth >= 8 && info->bestmoves[depth-1] != thread->pv.line[0])
+                info->idealusage = MIN(info->maxusage, info->idealusage + info->baseusage * 1.25);
         }
         
         // Check for termination by any of the possible limits
