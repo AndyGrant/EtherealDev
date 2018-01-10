1681f4417b04833ca6af968b44ea788505b0a213
AndyGrant <Andrew@grantnet.us>
2018-01-09 22:17:04 -0500

    .

--- src/search.c
+++ src/search.c
@@ -433,7 +433,6 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     // likely going to be good at a full depth. To save some work we will prune
     // captures that won't exceed rbeta or captures that fail at a low depth
     if (   !PvNode
-        && !inCheck
         &&  depth >= 5){
             
         int rbeta = MIN(beta + 150, MATE - MAX_HEIGHT - 1);
