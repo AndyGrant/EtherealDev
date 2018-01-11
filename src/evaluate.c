b869a2361fd583534c5ec765432c24abe4e80b8b
AndyGrant <Andrew@grantnet.us>
2018-01-11 02:07:53 -0500

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -645,7 +645,6 @@ void evaluateKings(EvalInfo* ei, Board* board, int colour){
                                                : MAX(1, MIN(4, kingRank - Rank(getmsb(filePawns))))
                                                : 0;
                                                
-        ei->midgame[colour] += KingShelter[file == File(kingSq)][distance];
         ei->endgame[colour] += KingShelter[file == File(kingSq)][distance];
     }
 }
