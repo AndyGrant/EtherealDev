b725405d708fa5a8ed29ecb81d824ab34995b06d
AndyGrant <Andrew@grantnet.us>
2018-01-12 05:30:30 -0500

    .

--- src/search.c
+++ src/search.c
@@ -758,7 +758,7 @@ int weakCapture(Board* board, EvalInfo* ei, uint16_t move){
     target   =   target == BISHOP ? KNIGHT :   target;
     attacker = attacker == BISHOP ? KNIGHT : attacker;
     
-    return (attacker != KING && target >= attacker);
+    return (attacker != KING && target < attacker);
 }
 
 
