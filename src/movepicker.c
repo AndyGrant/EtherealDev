27600573c4ed3e438ae6cb76107b7ac075f41c30
AndyGrant <Andrew@grantnet.us>
2018-01-12 18:07:50 -0500

    .

--- src/movepicker.c
+++ src/movepicker.c
@@ -219,9 +219,12 @@ void evaluateQuietMoves(MovePicker* mp, Board* board){
         move = mp->moves[i];
         
         // Use the history score and PSQT to evaluate the move
-        value =  getHistoryScore(*mp->history, move, board->turn, 512);
-        value += abs(PSQTMidgame[board->squares[MoveFrom(move)]][MoveTo(move)  ]);
-        value -= abs(PSQTMidgame[board->squares[MoveFrom(move)]][MoveFrom(move)]);
+        value = getHistoryScore(*mp->history, move, board->turn, 512);
+        
+        if (PieceType(board->squares[MoveFrom(move)]) != KING)
+            value += (board->turn == WHITE ? 1 : -1)
+                  * (PSQTMidgame[board->squares[MoveFrom(move)]][MoveTo(move)  ]
+                  -  PSQTMidgame[board->squares[MoveFrom(move)]][MoveFrom(move)]);
         
         mp->values[i] = value;
     }
