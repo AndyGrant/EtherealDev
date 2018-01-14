fd6b4a8958b8921f0d79d4c16b646ab794bffad9
AndyGrant <Andrew@grantnet.us>
2018-01-14 02:38:42 -0500

    .

--- src/search.c
+++ src/search.c
@@ -209,14 +209,14 @@ int aspirationWindow(Thread* thread, int depth){
     if (depth > 4 && abs(values[mainDepth-1]) < MATE / 2){
         
         // Dynamically compute the upper margin based on previous scores
-        upper = MAX(    4,  1.6 * (values[mainDepth-1] - values[mainDepth-2]));
-        upper = MAX(upper,  2.0 * (values[mainDepth-2] - values[mainDepth-3]));
-        upper = MAX(upper,  0.8 * (values[mainDepth-3] - values[mainDepth-4]));
+        upper = MAX(    4,  2.0 * (values[mainDepth-1] - values[mainDepth-2]));
+        upper = MAX(upper,  1.6 * (values[mainDepth-2] - values[mainDepth-3]));
+        upper = MAX(upper,  1.2 * (values[mainDepth-3] - values[mainDepth-4]));
         
         // Dynamically compute the lower margin based on previous scores
-        lower = MAX(    4, -1.6 * (values[mainDepth-1] - values[mainDepth-2]));
-        lower = MAX(lower, -2.0 * (values[mainDepth-2] - values[mainDepth-3]));
-        lower = MAX(lower, -0.8 * (values[mainDepth-3] - values[mainDepth-4])); 
+        lower = MAX(    4, -2.0 * (values[mainDepth-1] - values[mainDepth-2]));
+        lower = MAX(lower, -1.6 * (values[mainDepth-2] - values[mainDepth-3]));
+        lower = MAX(lower, -1.2 * (values[mainDepth-3] - values[mainDepth-4])); 
         
         // Create the aspiration window
         alpha = values[mainDepth-1] - lower;
