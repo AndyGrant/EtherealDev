8cff1a9775bfe5218a0444b8e2639689374d1b33
AndyGrant <Andrew@grantnet.us>
2018-01-16 21:58:54 -0500

    .

--- src/search.c
+++ src/search.c
@@ -384,7 +384,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         if (depth <= 1)
             return qsearch(thread, pv, alpha, beta, height);
         
-        rAlpha = alpha - RazorMargins[depth];
+        rAlpha = alpha - RazorMargins[depth] + 128;
         value = qsearch(thread, pv, rAlpha, rAlpha + 1, height);
         if (value <= rAlpha) return value;
     }
