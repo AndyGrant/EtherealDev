902f92d21356c18c76db6bb5327729a87bed66b4
AndyGrant <Andrew@grantnet.us>
2018-01-18 18:35:18 -0500

    .

--- src/search.c
+++ src/search.c
@@ -162,11 +162,11 @@ void* iterativeDeepening(void* vthread){
             
             // Increase our time if the score suddently dropped by eight centipawns
             if (depth >= 4 && info->values[depth - 1] > value + 8)
-                info->idealusage = MIN(info->maxusage, info->idealusage * 1.10);
+                info->idealusage *= 1.00 + (info->values[depth-1] - value) / 100.0;
             
             // Increase our time if the pv has changed across the last two iterations
             if (depth >= 4 && info->bestmoves[depth - 1] != thread->pv.line[0])
-                info->idealusage = MIN(info->maxusage, info->idealusage * 1.35);
+                info->idealusage *= 1.35;
         }
         
         // Check for termination by any of the possible limits
