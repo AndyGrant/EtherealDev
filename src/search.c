b50caef0a135c03f4fe1fd7ab17a3077b5226bd7
AndyGrant <Andrew@grantnet.us>
2018-01-17 19:42:58 -0500

    .

--- src/search.c
+++ src/search.c
@@ -64,8 +64,8 @@ uint16_t getBestMove(Thread* threads, Board* board, Limits* limits, double time,
         
         mtg = mtg >= 0 ? mtg + 1: 25;
         
-        info.idealusage = 0.30 * (time + (mtg - 2) * inc) / MAX(5, mtg);
-        info.maxusage   = 3.00 * (time + (mtg - 2) * inc) / MAX(5, mtg);
+        info.idealusage = 0.50 * (time + (mtg - 2) * inc) / MAX(5, mtg);
+        info.maxusage   = 4.50 * (time + (mtg - 2) * inc) / MAX(5, mtg);
         
         info.idealusage = MIN(info.idealusage, time - 20);
         info.maxusage   = MIN(info.maxusage,   time - 20);
