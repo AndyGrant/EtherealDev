97e3f47d14b2712d261d285d937a574f2610320a
AndyGrant <Andrew@grantnet.us>
2018-01-24 18:36:28 -0500

    .

--- src/search.c
+++ src/search.c
@@ -230,7 +230,7 @@ int aspirationWindow(Thread* thread, int depth){
         for (; lower <= 640 && upper <= 640; lower *= 2, upper *= 2){
             
             // Perform the search on the modified window
-            value = search(thread, &thread->pv, alpha, beta, depth, 0);
+            value = search(thread, &thread->pv, alpha, beta, depth, 0, 0);
             
             // Result was within our window
             if (value > alpha && value < beta)
@@ -255,10 +255,10 @@ int aspirationWindow(Thread* thread, int depth){
     }
     
     // Full window search when near mate or when depth is below or equal to 4
-    return search(thread, &thread->pv, -MATE, MATE, depth, 0);
+    return search(thread, &thread->pv, -MATE, MATE, depth, 0, 0);
 }
 
-int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int height){
+int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int height, int skipTTCutoff){
     
     const int PvNode   = (alpha != beta - 1);
     const int RootNode = (height == 0);
@@ -362,19 +362,39 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         // Step 6A. Check to see if this entry allows us to exit this
         // node early. We choose not to do this in the PV line, not because
         // we can't, but because don't want truncated PV lines
-        if (!PvNode && ttEntry.depth >= depth){
+        if (!PvNode && !skipTTCutoff){
 
             rAlpha = alpha; rBeta = beta;
             ttValue = valueFromTT(ttEntry.value, height);
             
             switch (ttEntry.type){
-                case  PVNODE: return ttValue;
+                case  PVNODE: rAlpha = MAX(alpha, ttValue);
+                              rBeta  = MIN(beta,  ttValue);  break;
                 case CUTNODE: rAlpha = ttValue > alpha ? ttValue : alpha; break;
-                case ALLNODE:  rBeta = ttValue <  beta ? ttValue :  beta; break;
+                case ALLNODE: rBeta  = ttValue <  beta ? ttValue :  beta; break;
             }
             
-            // Entry allows early exit
-            if (rAlpha >= rBeta) return ttValue;
+            if (rAlpha >= rBeta && ttEntry.depth >= depth) 
+                return ttValue;
+            
+            if (ttEntry.depth + 1 == depth && rAlpha - 64 >= rBeta) 
+                return ttValue;
+            
+            if (ttEntry.depth + 2 == depth && rAlpha - 64 >= rBeta){
+                
+                value = search(thread, pv, alpha, beta, depth - 1, height, 1);
+                
+                if (ttValue <= alpha && value <= alpha)
+                    return value;
+                
+                if (ttValue >= beta && value >= beta)
+                    return value;
+                
+                if (pv->length >= 1){
+                    ttMove = pv->line[0];
+                    ttTactical = moveIsTactical(board, ttMove);
+                }
+            }
         }
     }
     
@@ -395,6 +415,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     // move would close the massive gap between the evaluation and alpha
     if (   !PvNode
         && !inCheck
+        && !skipTTCutoff
         &&  depth <= RazorDepth
         &&  eval + RazorMargins[depth] < alpha){
             
@@ -410,6 +431,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     // Move Pruning. If the eval is few pawns above beta then exit early
     if (   !PvNode
         && !inCheck
+        && !skipTTCutoff
         &&  depth <= BetaPruningDepth
         &&  eval - 70 * depth > beta)
         return beta;
@@ -420,6 +442,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     // in saying that our position is too good to be true
     if (   !PvNode
         && !inCheck
+        && !skipTTCutoff
         &&  depth >= NullMovePruningDepth
         &&  eval >= beta
         &&  hasNonPawnMaterial(board, board->turn)
@@ -429,7 +452,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             
         applyNullMove(board, undo);
         
-        value = -search(thread, &lpv, -beta, -beta + 1, depth - R, height + 1);
+        value = -search(thread, &lpv, -beta, -beta + 1, depth - R, height + 1, 0);
         
         revertNullMove(board, undo);
         
@@ -442,6 +465,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     // captures that won't exceed rbeta or captures that fail at a low depth
     if (   !PvNode
         && !inCheck
+        && !skipTTCutoff
         &&  depth >= 5){
             
         int rbeta = MIN(beta + 150, MATE - MAX_HEIGHT - 1);
@@ -465,8 +489,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // Verify the move is good with a depth zero search (qsearch, unless in check)
             // and then with a slightly reduced search. If both searches still exceed rbeta,
             // we will prune this node's subtree with resonable assurance that we made no error
-            if (   -search(thread, &lpv, -rbeta, -rbeta+1,       0, height+1) >= rbeta
-                && -search(thread, &lpv, -rbeta, -rbeta+1, depth-4, height+1) >= rbeta){
+            if (   -search(thread, &lpv, -rbeta, -rbeta+1,       0, height+1, 0) >= rbeta
+                && -search(thread, &lpv, -rbeta, -rbeta+1, depth-4, height+1, 0) >= rbeta){
                     
                 revertMove(board, currentMove, undo);
                 return beta;
@@ -484,7 +508,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         &&  depth >= InternalIterativeDeepeningDepth){
         
         // Search with a reduced depth
-        value = search(thread, &lpv, alpha, beta, depth-2, height);
+        value = search(thread, &lpv, alpha, beta, depth-2, height, 0);
         
         // Probe for the newly found move, and update ttMove / ttTactical
         if (getTranspositionEntry(&Table, board->hash, &ttEntry)){
@@ -590,13 +614,13 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         
         // Search the move with a possibly reduced depth, on a full or null window
         value =  (played == 1 || !PvNode)
-               ? -search(thread, &lpv,    -beta, -alpha, depth-R, height+1)
-               : -search(thread, &lpv, -alpha-1, -alpha, depth-R, height+1);
+               ? -search(thread, &lpv,    -beta, -alpha, depth-R, height+1, 0)
+               : -search(thread, &lpv, -alpha-1, -alpha, depth-R, height+1, 0);
                
         // If the search beat alpha, we may need to research, in the event that
         // the previous search was not the full window, or was a reduced depth
         value =  (value > alpha && (R != 1 || (played != 1 && PvNode)))
-               ? -search(thread, &lpv, -beta, -alpha, depth-1, height+1)
+               ? -search(thread, &lpv, -beta, -alpha, depth-1, height+1, 0)
                :  value;
         
         // REVERT MOVE FROM BOARD
