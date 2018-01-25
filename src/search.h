be100f60f54b1a8120502f97e9b2ee9a6de801e8
AndyGrant <Andrew@grantnet.us>
2018-01-24 20:47:41 -0500

    .

--- src/search.h
+++ src/search.h
@@ -48,7 +48,7 @@ void* iterativeDeepening(void* vthread);
 
 int aspirationWindow(Thread* thread, int depth);
 
-int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int height);
+int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int height, int cutNode, int skipTTCutoff);
 
 int qsearch(Thread* thread, PVariation* pv, int alpha, int beta, int height);
 
