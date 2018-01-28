9a040627576e5244ec7ea16210c80d80997740cc
AndyGrant <Andrew@grantnet.us>
2018-01-27 22:25:16 -0500

    .

--- src/search.c
+++ src/search.c
@@ -579,8 +579,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             R += (depth  - 4) / 6;
             R += 2 * !PvNode;
             R += ttTactical && bestMove == ttMove;
-            R += hist < -8192 ? 1 : 0;
-            R -= hist > 0 ? hist / 2048 : 0;
+            R += hist <= -8192 ? 1 : 0;
+            R -= hist >= -8192 ? (hist + 8192) / 4096 : 0;
             R  = MIN(depth - 1, MAX(R, 1));
         }
         
