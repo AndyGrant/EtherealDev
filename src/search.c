6c8f6736de00363d323e81f55bb73ce0ffae4d0a
AndyGrant <Andrew@grantnet.us>
2018-01-28 23:09:32 -0500

    .

--- src/search.c
+++ src/search.c
@@ -62,13 +62,18 @@ uint16_t getBestMove(Thread* threads, Board* board, Limits* limits, double time,
     // Ethereal is responsible for choosing how much time to spend searching
     if (limits->limitedBySelf){
         
-        mtg = mtg >= 0 ? mtg + 5: 25;
+        if (mtg >= 0){
+            info.idealusage = 0.50 * time / (mtg + 8) + inc;
+            info.maxusage   = 4.50 * time / (mtg + 5) + inc;
+        }
         
-        info.idealusage = 0.50 * time / (mtg + 3) + inc;
-        info.maxusage   = 4.50 * time / (mtg + 0) + inc;
+        else {
+            info.idealusage =  0.45 * (time + 23 * inc) / 28;
+            info.maxusage   = 10.00 * (time + 23 * inc) / 25;
+        }
         
-        info.idealusage = MIN(info.idealusage, time - 50);
-        info.maxusage   = MIN(info.maxusage,   time - 50);
+        info.idealusage = MIN(info.idealusage, time - 100);
+        info.maxusage   = MIN(info.maxusage,   time -  50);
     }
     
     // UCI command told us to look for exactly X seconds
