e0e7050850e6dd504b96b68cf11d2a8e8b3c0c2f
AndyGrant <Andrew@grantnet.us>
2018-01-29 21:59:39 -0500

    .

--- src/search.c
+++ src/search.c
@@ -182,13 +182,13 @@ void* iterativeDeepening(void* vthread){
             
             // Increase our time if the pv has changed across the last two iterations
             if (info->bestmoves[depth-1] != thread->pv.line[0])
-                info->idealusage = MIN(info->maxusage, info->idealusage * MAX(info->pvStability, 1.30));
+                info->idealusage = MIN(info->maxusage, info->idealusage * MAX(info->pvStability, 1.00));
             
             // delta = value - info->values[depth-1];
             // info->scoreStability *= delta < -8 ? 1.20 : 0.98;
             
             delta = info->bestmoves[depth-1] != thread->pv.line[0];
-            info->pvStability *= delta ? 0.60 : 1.10;
+            info->pvStability *= delta ? 0.85 : 1.05;
         }
         
         // Check for termination by any of the possible limits
