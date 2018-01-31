9e288541d8cddfefcb6537aa921be543b21f2668
AndyGrant <Andrew@grantnet.us>
2018-01-30 22:06:37 -0500

    .

--- src/search.c
+++ src/search.c
@@ -189,10 +189,10 @@ void* iterativeDeepening(void* vthread){
         // Check to see if we expect to be able to complete the next depth
         if (thread->limits->limitedBySelf){
             double timeFactor = info->timeUsage[depth] / MAX(1, info->timeUsage[depth-1]);
-            double estimatedUsage = info->timeUsage[depth] * (timeFactor + .40);
+            double estimatedUsage = info->timeUsage[depth] * timeFactor;
             double estiamtedEndtime = getRealTime() + estimatedUsage - info->starttime;
             
-            if (estiamtedEndtime > info->maxusage){
+            if (estiamtedEndtime > info->idealusage){
                 
                 // Terminate all helper threads
                 for (i = 0; i < thread->nthreads; i++)
