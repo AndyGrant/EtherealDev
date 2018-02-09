7f9d9afaf5ebd625367c169ed4f9b663edb26655
AndyGrant <Andrew@grantnet.us>
2018-02-08 21:50:13 -0500

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -147,17 +147,19 @@ const int PassedPawn[2][2][RANK_NB][PHASE_NB] = {
    {{   0,   0}, {  -2,   0}, { -12,   5}, { -15,  29}, {  -1,  68}, {  50, 157}, { 139, 254}, {   0,   0}}},
 };
 
-const int KingSafety[100] = { // Taken from CPW / Stockfish
-       0,   0,   1,   2,   3,   5,   7,   9,  12,  15,
-      18,  22,  26,  30,  35,  39,  44,  50,  56,  62,
-      68,  75,  82,  85,  89,  97, 105, 113, 122, 131,
-     140, 150, 169, 180, 191, 202, 213, 225, 237, 248,
-     260, 272, 283, 295, 307, 319, 330, 342, 354, 366,
-     377, 389, 401, 412, 424, 436, 448, 459, 471, 483,
-     494, 500, 500, 500, 500, 500, 500, 500, 500, 500,
-     500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
-     500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
-     500, 500, 500, 500, 500, 500, 500, 500, 500, 500
+const int KingSafety[96] = {
+      -17,   -13,   -10,    -7,    -6,    -4,    -2,     0,
+        0,     0,     0,     0,     0,    -1,    -2,    -3,
+       -4,    -6,    -8,   -10,   -14,   -16,   -19,   -24,
+      -28,   -32,   -37,   -41,   -46,   -53,   -58,   -64,
+      -70,   -77,   -84,   -91,   -99,  -107,  -114,  -124,
+     -133,  -141,  -150,  -160,  -169,  -179,  -190,  -200,
+     -211,  -222,  -233,  -245,  -256,  -269,  -281,  -293,
+     -307,  -320,  -333,  -346,  -361,  -374,  -389,  -404,
+     -418,  -433,  -449,  -464,  -480,  -496,  -512,  -520,
+     -520,  -520,  -520,  -520,  -520,  -520,  -520,  -520,
+     -520,  -520,  -520,  -520,  -520,  -520,  -520,  -520,
+     -520,  -520,  -520,  -520,  -520,  -520,  -520,  -520,
 };
 
 const int NoneValue[PHASE_NB] = {   0,   0};
@@ -633,15 +635,20 @@ void evaluateKings(EvalInfo* ei, Board* board, int colour){
     ei->midgame[colour] += KingDefenders[defenderCounts][MG];
     ei->endgame[colour] += KingDefenders[defenderCounts][EG];
     if (TRACE) T.kingDefenders[colour][defenderCounts]++;
-
-    // Scale down attack count if there are no enemy queens
-    attackCounts  = ei->attackCounts[!colour];
-    if (!(board->colours[!colour] & board->pieces[QUEEN]))
-        attackCounts *= .25;
     
-    if (TRACE) T.kingSafety[colour][0] += attackCounts;
-    if (TRACE) T.kingSafety[colour][1] += attackCounts * attackCounts;
-    if (TRACE) T.kingSafety[colour][2] += attackCounts * attackCounts * attackCounts;
+    if (ei->attackCounts[!colour] >= 2){
+        // Scale down attack count if there are no enemy queens
+        attackCounts  = ei->attackCounts[!colour];
+        if (!(board->colours[!colour] & board->pieces[QUEEN]))
+            attackCounts *= .25;
+        attackCounts = MIN(attackCounts, 95);
+        
+        ei->midgame[colour] += KingSafety[attackCounts];
+        ei->endgame[colour] += KingSafety[attackCounts];
+        if (TRACE) T.kingSafety[colour][0] += attackCounts;
+        if (TRACE) T.kingSafety[colour][1] += attackCounts * attackCounts;
+        if (TRACE) T.kingSafety[colour][2] += attackCounts * attackCounts * attackCounts;
+    }
     
     // Evaluate Pawn Shelter. We will evaluate the pawn setup on the king's file,
     // as well as the files next to the king (if there are any). We based the evaluation
