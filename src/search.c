600d23b8e58f7e54bd222e9c9a38f06dce7af38d
AndyGrant <Andrew@grantnet.us>
2018-02-09 00:35:38 -0500

    .

--- src/search.c
+++ src/search.c
@@ -703,7 +703,7 @@ int qsearch(Thread* thread, PVariation* pv, int alpha, int beta, int height){
     
     Board* const board = &thread->board;
     
-    int eval, value, best, maxValueGain;
+    int eval, value, best;
     uint16_t currentMove;
     Undo undo[1];
     MovePicker movePicker;
@@ -739,22 +739,7 @@ int qsearch(Thread* thread, PVariation* pv, int alpha, int beta, int height){
     if (value > alpha) alpha = value;
     
     // QSearch can be terminated
-    if (alpha >= beta) return value;
-    
-    // Take a guess at the best case gain for a non promotion capture
-    if (board->colours[!board->turn] & board->pieces[QUEEN])
-        maxValueGain = PieceValues[QUEEN ][EG] + 55;
-    else if (board->colours[!board->turn] & board->pieces[ROOK])
-        maxValueGain = PieceValues[ROOK  ][EG] + 35;
-    else
-        maxValueGain = PieceValues[BISHOP][EG] + 15;
-    
-    // Delta pruning when no promotions and not extreme late game
-    if (     value + maxValueGain < alpha
-        && !(board->colours[WHITE] & board->pieces[PAWN] & RANK_7)
-        && !(board->colours[BLACK] & board->pieces[PAWN] & RANK_2))
-        return value;
-    
+    if (alpha >= beta) return value;    
     
     initializeMovePicker(&movePicker, thread, NONE_MOVE, height, 1);
     
