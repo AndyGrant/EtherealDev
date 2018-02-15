4c11d22163477f24123f8ef32f28b788931b685e
AndyGrant <Andrew@grantnet.us>
2018-02-15 01:29:10 -0500

    .

--- src/search.c
+++ src/search.c
@@ -613,6 +613,10 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // Increase R by an additional two ply for non PvNodes
             R += 2 * !PvNode;
             
+            R -=    !inCheck 
+                 && !isNotInCheck(board, board->turn)
+                 && squareIsAttacked(board, !board->turn, MoveTo(currentMove));
+            
             // Decrease R by an additional ply if we have a quiet move as our best
             // move, or we are looking at an early quiet move in a situation where
             // we either have no table move, or the table move is not the best so far
