44c1f30ad8b26d6b4b6868a4a67e43bd78c7dc79
AndyGrant <Andrew@grantnet.us>
2018-02-17 02:44:18 -0500

    .

--- src/search.h
+++ src/search.h
@@ -87,7 +87,7 @@ static const int FutilityPruningDepth = 8;
 
 static const int LateMovePruningDepth = 8;
 
-static const int LateMovePruningCounts[] = {0, 5, 7, 11, 16, 24, 33, 43, 56};
+static const int LateMovePruningCounts[] = {0, 4, 7, 10, 15, 22, 30, 39, 52};
 
 static const int QFutilityMargin = 55;
 
