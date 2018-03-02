4b6077926ddd128980330fddde5ec7e56e2c5ec2
AndyGrant <Andrew@grantnet.us>
2018-03-02 16:15:50 -0500

    .

--- src/history.c
+++ src/history.c
@@ -30,8 +30,7 @@ void updateHistory(HistoryTable history, uint16_t move, int colour, int delta){
     int to    = MoveTo(move);
     int entry = history[colour][from][to];
     
-    // Ensure the update value is within [-400, 400]
-    delta = MAX(-400, MIN(400, delta));
+    if (delta > 225 || delta < -225) return;
     
     // Ensure the new value is within [-16384, 16384]
     entry += 32 * delta - entry * abs(delta) / 512;
