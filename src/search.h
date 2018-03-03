a2a6212c14b99a5c1d0623a3ddc93778e5158762
AndyGrant <Andrew@grantnet.us>
2018-03-02 22:25:17 -0500

    .

--- src/search.h
+++ src/search.h
@@ -69,9 +69,9 @@ int thisTacticalMoveValue(Board* board, uint16_t move);
 int bestTacticalMoveValue(Board* board);
 
 
-static const int RazorDepth = 4;
+static const int RazorDepth = 3;
 
-static const int RazorMargins[] = {0, 450, 480, 520, 580};
+static const int RazorMargin = 150;
 
 static const int BetaPruningDepth = 8;
 
