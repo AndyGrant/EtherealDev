b7c83e24bb93ae88cb062dde9894140b13405ba9
AndyGrant <Andrew@grantnet.us>
2018-03-06 01:15:29 -0500

    .

--- src/search.c
+++ src/search.c
@@ -601,7 +601,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         // Step 17. Late Move Reductions. We will search some moves at a
         // lower depth. If they look poor at a lower depth, then we will
         // move on. If they look good, we will search with a full depth.
-        if (    played > 1
+        if (   (played >= 4 || (played > 1 && !PvNode))
             &&  depth >= 3
             &&  isQuiet){
             
