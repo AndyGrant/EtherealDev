ad1c56ed7473228d32ebe18131c8f7e71d5725d0
AndyGrant <Andrew@grantnet.us>
2018-03-09 23:17:10 -0500

    .

--- src/evaluate.h
+++ src/evaluate.h
@@ -23,9 +23,9 @@
 
 #define SCALE_FACTOR        (100)
 #define SCALE_OCB_Q_OR_N    (105)
-#define SCALE_OCB_BRVBR     ( 90)
-#define SCALE_OCB_BVB       ( 70)
-#define SCALE_OCB_ROOK_ADV  (110)
+#define SCALE_OCB_BRVBR     ( 95)
+#define SCALE_OCB_BVB       ( 85)
+#define SCALE_OCB_ROOK_ADV  (105)
 
 typedef struct EvalTrace {
     
