d4c87c6ab17d06b72962bf43a97f6431677bec2d
AndyGrant <Andrew@grantnet.us>
2018-03-13 14:59:08 -0400

    .

--- src/search.c
+++ src/search.c
@@ -65,9 +65,9 @@ uint16_t getBestMove(Thread* threads, Board* board, Limits* limits, double time,
     if (limits->limitedBySelf){
         
         if (mtg >= 0){
-            info.idealusage =  0.65 * time / (mtg +  5) + inc;
-            info.maxalloc   =  4.00 * time / (mtg +  7) + inc;
-            info.maxusage   = 10.00 * time / (mtg + 10) + inc;
+            info.idealusage =  0.65 * time / (mtg + 5) + inc;
+            info.maxalloc   =  2.00 * time / (mtg + 5) + inc;
+            info.maxusage   =  4.00 * time / (mtg + 5) + inc;
         }
         
         else {
