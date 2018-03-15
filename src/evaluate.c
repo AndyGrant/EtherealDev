5b660c765ccb3efdce475c8f0051ca50e1d195af
AndyGrant <Andrew@grantnet.us>
2018-03-15 00:13:42 -0400

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -165,7 +165,7 @@ const int PassedPawn[2][2][RANK_NB][PHASE_NB] = {
 };
 
 
-const int KingThreatWeight[PIECE_NB] = {   4,   8,   8,  12,  16,   0};
+const int KingThreatWeight[PIECE_NB] = {   3,   7,   7,  11,  15,   0};
 
 const int KingThreatMissingPawn = 8;
 
