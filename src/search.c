f0c2335925a9dc6edb2ca468c80646b7d4db47ac
AndyGrant <Andrew@grantnet.us>
2018-03-21 18:56:17 -0400

    .

--- src/search.c
+++ src/search.c
@@ -422,7 +422,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     }
     
     // Step 13. Check Extension at non Root nodes that are PV or low depth
-    depth += inCheck && !RootNode && (PvNode || depth <= 6);
+    depth += inCheck && !RootNode && (PvNode || depth <= 10);
     
     
     initializeMovePicker(&movePicker, thread, ttMove, height, 0);
