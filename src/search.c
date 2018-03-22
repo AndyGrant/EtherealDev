de92535ab3f92f0c63e567751628c2bdbabfd26d
AndyGrant <Andrew@grantnet.us>
2018-03-22 14:33:26 -0400

    .

--- src/search.c
+++ src/search.c
@@ -148,14 +148,14 @@ int aspirationWindow(Thread* thread, int depth){
     if (depth > 4 && abs(values[mainDepth-1]) < MATE / 2){
         
         // Dynamically compute the upper margin based on previous scores
-        upper = MAX(   12,  1.6 * (values[mainDepth-1] - values[mainDepth-2]));
-        upper = MAX(upper,  1.3 * (values[mainDepth-2] - values[mainDepth-3]));
-        upper = MAX(upper,  1.0 * (values[mainDepth-3] - values[mainDepth-4]));
+        upper = MAX(    8, abs(values[mainDepth-1] - values[mainDepth-2]));
+        upper = MAX(upper, abs(values[mainDepth-2] - values[mainDepth-3]));
+        upper = MAX(upper, abs(values[mainDepth-3] - values[mainDepth-4]));
         
         // Dynamically compute the lower margin based on previous scores
-        lower = MAX(   12, -1.6 * (values[mainDepth-1] - values[mainDepth-2]));
-        lower = MAX(lower, -1.3 * (values[mainDepth-2] - values[mainDepth-3]));
-        lower = MAX(lower, -1.0 * (values[mainDepth-3] - values[mainDepth-4])); 
+        lower = MAX(    8, abs(values[mainDepth-1] - values[mainDepth-2]));
+        lower = MAX(lower, abs(values[mainDepth-2] - values[mainDepth-3]));
+        lower = MAX(lower, abs(values[mainDepth-3] - values[mainDepth-4])); 
         
         // Create the aspiration window
         alpha = values[mainDepth-1] - lower;
@@ -177,7 +177,7 @@ int aspirationWindow(Thread* thread, int depth){
             
             // Search failed high
             if (value >= beta)
-                beta  = beta + 2 * upper;
+                beta = beta + 2 * upper;
             
             // Result was a near mate score, force a full search
             if (abs(value) > MATE / 2)
