8ecc98ee2121d0dabf8d25d25cb55b6930a1b1be
AndyGrant <Andrew@grantnet.us>
2018-03-28 18:56:54 -0400

    .

--- src/search.c
+++ src/search.c
@@ -182,7 +182,7 @@ void* iterativeDeepening(void* vthread){
                 
                 // If we still have remaining increments from best move
                 // changes reduce our ideal time usage by a factor of 5%
-                info->idealusage *= info->bestMoveChanges ? 0.935 : 1.000;
+                info->idealusage *= info->bestMoveChanges ? 0.906 : 1.000;
                 
                 // Reduce our best move change debt
                 info->bestMoveChanges = MAX(0, info->bestMoveChanges - 1);
@@ -194,10 +194,10 @@ void* iterativeDeepening(void* vthread){
                 // first PV change in some time, we increase our time by 30%. If we
                 // have recently changed best moves, we will only adjust our usage
                 // to get back to the initial 30% time allocation by the first change
-                info->idealusage *= 1.000 + 0.080 * (6 - info->bestMoveChanges);
+                info->idealusage *= 1.000 + 0.120 * (4 - info->bestMoveChanges);
                 
                 // Set out counter back to six as the best move has changed
-                info->bestMoveChanges = 6;
+                info->bestMoveChanges = 4;
             }
             
             // Cap our ideal usage using our maximum allocation
