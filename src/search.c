26c22932c2143e9bc72aa6e956f3db39424641c9
AndyGrant <Andrew@grantnet.us>
2018-04-02 01:24:31 -0400

    .

--- src/search.c
+++ src/search.c
@@ -513,7 +513,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // Verify the move is good with a depth zero search (qsearch, unless in check)
             // and then with a slightly reduced search. If both searches still exceed rBeta,
             // we will prune this node's subtree with resonable assurance that we made no error
-            if (   -search(thread, &lpv, -rBeta, -rBeta+1,       0, height+1) >= rBeta
+            if (   -search(thread, &lpv, -rBeta, -rBeta+1, MAX(0, depth-8), height+1) >= rBeta
                 && -search(thread, &lpv, -rBeta, -rBeta+1, depth-4, height+1) >= rBeta){
                     
                 revertMove(board, currentMove, undo);
