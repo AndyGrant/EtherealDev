faf407bd61a6383a8fa23b329b5052205dad3786
AndyGrant <Andrew@grantnet.us>
2018-04-06 00:54:32 -0400

    .

--- src/search.h
+++ src/search.h
@@ -97,7 +97,7 @@ static const int FutilityPruningDepth = 8;
 static const int LateMovePruningDepth = 8;
 
 static const int LateMovePruningCounts[2][9] = {
-    {  0,  5,  7, 11, 16, 24, 33, 43, 56},
+    {  0,  5,  7, 10, 15, 23, 32, 42, 55},
     {  0,  7,  9, 13, 18, 26, 35, 45, 58},
 };
 
