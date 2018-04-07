10ac7135531dd3b827784d189bb1fb8b9e5ce4a6
AndyGrant <Andrew@grantnet.us>
2018-04-07 17:21:18 -0400

    .

--- src/search.c
+++ src/search.c
@@ -578,6 +578,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         if (   !PvNode
             &&  isQuiet
             &&  best > MATED_IN_MAX
+            && (hist < 8192 || !improving)
             &&  depth <= LateMovePruningDepth
             &&  quiets > LateMovePruningCounts[depth])
             break;
