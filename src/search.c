b1352d52fcc7113a3bf4a830cc6c38489879e8dc
AndyGrant <Andrew@grantnet.us>
2018-04-16 21:21:28 -0400

    .

--- src/search.c
+++ src/search.c
@@ -640,9 +640,15 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
                   &&  ttEntry.depth >= depth - 3
                   && (ttEntry.type == PVNODE || ttEntry.type == CUTNODE)
                   &&  moveIsSingular(thread, board, &ttEntry, undo, depth, height);
+                  
+        int localCheckExtend =    PvNode
+                              &&  inCheck
+                              && !singular
+                              && !checkExtended
+                              && (hist >= 2048 || !isQuiet);
             
         // New depth is what our search depth would be, assuming that we do no LMR
-        newDepth = depth + singular;
+        newDepth = depth + singular + localCheckExtend;
         
         // Step 17A. If we triggered the LMR conditions (which we know by the value of R),
         // then we will perform a reduced search on the null alpha window, as we have no
