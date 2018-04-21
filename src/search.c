793191ae5bfcb7dbab601b19b672167c2074b163
AndyGrant <Andrew@grantnet.us>
2018-04-21 01:41:56 -0400

    .

--- src/search.c
+++ src/search.c
@@ -610,6 +610,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // Baseline R based on number of moves played and current depth
             R = 2 + (played - 4) / 8 + (depth - 6) / 4;
             
+            R += improving && !bestWasQuiet && hist <= -4096;
+            
             // Increase R by an additional two ply for non PvNodes
             R += 2 * !PvNode;
             
