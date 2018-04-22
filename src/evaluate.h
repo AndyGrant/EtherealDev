f453a073be41906bb6f7907ae37252fc67c446bf
AndyGrant <Andrew@grantnet.us>
2018-04-22 13:41:31 -0400

    .

--- src/evaluate.h
+++ src/evaluate.h
@@ -55,7 +55,7 @@ struct EvalTrace {
     
     int kingPSQT[COLOUR_NB][SQUARE_NB];
     int kingDefenders[COLOUR_NB][12];
-    int kingShelter[COLOUR_NB][2][FILE_NB][RANK_NB];
+    int kingShelter[COLOUR_NB][2][RANK_NB];
     
     int passedPawn[COLOUR_NB][2][2][RANK_NB];
     
