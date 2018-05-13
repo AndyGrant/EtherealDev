fb5148f3fa9d7d0f017b3c55c973549b92e757ba
AndyGrant <Andrew@grantnet.us>
2018-05-13 02:21:52 -0400

    .

--- src/search.c
+++ src/search.c
@@ -646,10 +646,9 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         // positions, so long as on other extension has been made and the move
         // has resonable history. 
         extension +=  inCheck
-                  && !isQuiet
                   && !extension
                   && !checkExtended
-                  &&  hist >= 4096;
+                  && (!isQuiet || hist >= 4096);
             
         // New depth is what our search depth would be, assuming that we do no LMR
         newDepth = depth + extension;
