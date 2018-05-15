1af382994c440dfb2ed0d9099c4dc2f548ae0535
AndyGrant <Andrew@grantnet.us>
2018-05-15 14:18:44 -0400

    .

--- src/texel.h
+++ src/texel.h
@@ -1,17 +1,17 @@
 /*
   Ethereal is a UCI chess playing engine authored by Andrew Grant.
   <https://github.com/AndyGrant/Ethereal>     <andrew@grantnet.us>
-  
+
   Ethereal is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   Ethereal is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -22,7 +22,7 @@
 #include "types.h"
 
 // Depth for evaluating each position
-#define TEXEL_DEPTH (0)
+#define TEXEL_DEPTH (2)
 
 // Number of Positions in the data set
 #define NP (1470000)
@@ -56,28 +56,28 @@
 // Define the Knight Terms
 #define TuneKnightRammedPawns (0 * 1)
 #define TuneKnightOutpost     (0 * 2)
-#define TuneKnightMobility    (0 * 9)
+#define TuneKnightMobility    (1 * 9)
 
 // Define the Bishop Terms
 #define TuneBishopPair        (0 *  1)
 #define TuneBishopRammedPawns (0 *  1)
 #define TuneBishopOutpost     (0 *  2)
-#define TuneBishopMobility    (0 * 14)
+#define TuneBishopMobility    (1 * 14)
 
 // Define the Rook Terms
 #define TuneRookFile      (0 *  2)
 #define TuneRookOnSeventh (0 *  1)
-#define TuneRookMobility  (0 * 15)
+#define TuneRookMobility  (1 * 15)
 
 // Define the Queen Terms
-#define TuneQueenMobility (0 * 28)
+#define TuneQueenMobility (1 * 28)
 
 // Define the King Terms
 #define TuneKingDefenders (0 *  12)
 #define TuneKingShelter   (0 * 128)
 
 // Define the Passed Pawn Terms
-#define TunePassedPawn (1 * 32)
+#define TunePassedPawn (0 * 32)
 
 // Define the Threat Terms
 #define TuneThreatPawnAttackedByOne     (0 * 1)
