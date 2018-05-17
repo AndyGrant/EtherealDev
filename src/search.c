4d1268de2a800e23d8cd9d065c2b06604eaa3e74
AndyGrant <Andrew@grantnet.us>
2018-05-17 03:55:40 -0400

    .

--- src/search.c
+++ src/search.c
@@ -448,7 +448,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
 
         revertNullMove(board, undo);
 
-        if (value >= beta) return value;
+        if (value >= beta) return value >= MATE_IN_MAX ? beta : value;
     }
 
     // Step 10. ProbCut. If we have a good capture that causes a beta cutoff
