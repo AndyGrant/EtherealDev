3dec0cb35087b6d0da1f590fae4dc00499290edb
AndyGrant <Andrew@grantnet.us>
2018-05-19 04:35:08 -0400

    .

--- src/search.h
+++ src/search.h
@@ -93,7 +93,7 @@ static const int LateMovePruningDepth = 8;
 
 static const int LateMovePruningCounts[] = {0, 4, 6, 10, 15, 23, 31, 40, 52};
 
-static const int SEEMargin = 25;
+static const int SEEMargin = -25;
 
 static const int QFutilityMargin = 100;
 
