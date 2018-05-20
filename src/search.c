b12d3a94bcbef32d3e8cc3e583e18d860b302037
AndyGrant <Andrew@grantnet.us>
2018-05-20 19:16:58 -0400

    .

--- src/search.c
+++ src/search.c
@@ -580,11 +580,9 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
 
             R -= quiets <= 3; // Reduce for first few quiets
 
-            // Adjust based on the history score, within [+1, -6]
-            R -= MAX(-1, ((hist + 8192) / 4096) - (hist <= -8192));
+            R -= (hist + 8192) / 4096; // Adjust by history
 
-            // Don't extend the search and don't go into qsearch
-            R = MIN(depth - 1, MAX(R, 1));
+            R = MIN(depth - 1, MAX(R, 1)); // Don't extend or go into qsearch
 
         } else R = 1;
 
