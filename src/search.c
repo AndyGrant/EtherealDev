0b3ad5d9fab38c952b2bc3570088d6f4fbd12dd3
AndyGrant <Andrew@grantnet.us>
2018-06-12 01:53:58 -0400

    .

--- src/search.c
+++ src/search.c
@@ -537,7 +537,6 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         if (   !PvNode
             &&  isQuiet
             &&  best > MATED_IN_MAX
-            && (hist < 4096 || !improving)
             &&  futilityMargin <= alpha
             &&  depth <= FutilityPruningDepth)
             break;
