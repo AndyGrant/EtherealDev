22a4e6145f64dd2f0a02645f0ae6cf007ac47394
AndyGrant <Andrew@grantnet.us>
2018-06-12 01:59:25 -0400

    .

--- src/time.c
+++ src/time.c
@@ -75,7 +75,7 @@ void initializeTimeManagment(SearchInfo* info, Limits* limits){
 
         // Playing using X / Y or X / Y + Z time controls
         if (limits->mtg >= 0){
-            info->idealUsage =  0.65 * limits->time / (limits->mtg +  5) + limits->inc;
+            info->idealUsage =  0.85 * limits->time / (limits->mtg +  5) + limits->inc;
             info->maxAlloc   =  4.00 * limits->time / (limits->mtg +  7) + limits->inc;
             info->maxUsage   = 10.00 * limits->time / (limits->mtg + 10) + limits->inc;
         }
