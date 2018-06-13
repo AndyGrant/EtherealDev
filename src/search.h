80cce3ac4e5b593d07bade995dc950cd4f1cc197
AndyGrant <Andrew@grantnet.us>
2018-06-12 22:31:20 -0400

    .

--- src/search.h
+++ src/search.h
@@ -85,7 +85,7 @@ static const int NullMovePruningDepth = 2;
 
 static const int ProbCutDepth = 5;
 
-static const int ProbCutMargin = 100;
+static const int ProbCutMargin = 135;
 
 static const int IIDDepth = 3;
 
