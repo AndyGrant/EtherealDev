de7e057ea24539d6862dd1f333dd85decab3b2af
AndyGrant <Andrew@grantnet.us>
2018-06-19 00:05:51 -0400

    .

--- src/search.h
+++ src/search.h
@@ -72,7 +72,7 @@ int moveIsSingular(Thread* thread, uint16_t ttMove, int ttValue, Undo* undo, int
 
 static const int RazorDepth = 4;
 
-static const int RazorMargins[] = {0, 300, 350, 410, 500};
+static const int RazorMargins[] = {0, 250, 300, 350, 450};
 
 static const int BetaPruningDepth = 8;
 
