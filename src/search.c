f48af5e6c349e3039d8b19c275d6cbda8e64a6f6
AndyGrant <Andrew@grantnet.us>
2018-06-24 03:11:55 -0400

    .

--- src/search.c
+++ src/search.c
@@ -261,7 +261,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     int quiets = 0, played = 0, hist = 0;
     int ttHit, ttValue = 0, ttEval = 0, ttDepth = 0, ttBound = 0;
     int i, reps, R, newDepth, rAlpha, rBeta, oldAlpha = alpha;
-    int inCheck, isQuiet, improving, checkExtended, extension;
+    int inCheck, isQuiet, improving, extension;
     int eval, value = -MATE, best = -MATE, futilityMargin = -MATE;
     uint16_t move, ttMove = NONE_MOVE, bestMove = NONE_MOVE, quietsTried[MAX_MOVES];
 
@@ -401,16 +401,10 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     // We can grab in check based on the already computed king attackers bitboard
     inCheck = !!board->kingAttackers;
 
-    // Here we perform our check extension, for non-root pvnodes, or for non-root
-    // nodes near depth zero. Note that when we bypass the qsearch as a result of
-    // being in check, we set depth to zero. This step adjusts depth back to one.
-    checkExtended = inCheck && !RootNode && depth <= 8;
-    depth += inCheck && !RootNode && depth <= 8;
-
     // Compute and save off a static evaluation. Also, compute our futilityMargin
     eval = thread->evalStack[height] = ttHit && ttEval != VALUE_NONE ? ttEval
                                      : evaluateBoard(board, &thread->pktable);
-    futilityMargin = eval + FutilityMargin * depth;
+    futilityMargin = eval + FutilityMargin * (depth + inCheck);
 
     // Improving if our static eval increased in the last move
     improving = height >= 2 && eval > thread->evalStack[height-2];
@@ -609,7 +603,6 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         // and it seems that under some conditions, the table move is better than
         // all other possible moves, we will extend the search of the table move
         extension =  !RootNode
-                  && !checkExtended
                   &&  depth >= 10
                   &&  move == ttMove
                   &&  ttDepth >= depth - 3
@@ -618,10 +611,10 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
 
         // Step 17B. Check Extensions. We extend captures from any in
         // check positions, so long as no other extension has been made
-        extension +=  inCheck
-                  && !isQuiet
+        extension += !RootNode
                   && !extension
-                  && !checkExtended;
+                  &&  board->kingAttackers
+                  && (!isQuiet || hist > 4096);
 
         // New depth is what our search depth would be, assuming that we do no LMR
         newDepth = depth + extension;
