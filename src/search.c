8727f3fa06730aaab037614a73e97641f35aa582
AndyGrant <Andrew@grantnet.us>
2018-06-23 23:21:31 -0400

    No extend check at root. No bench check because no incheck benches

--- src/search.c
+++ src/search.c
@@ -618,7 +618,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
 
         // Step 17B. Check Extensions. We extend captures from any in
         // check positions, so long as no other extension has been made
-        extension +=  inCheck
+        extension += !RootNode
+                  &&  inCheck
                   && !isQuiet
                   && !extension
                   && !checkExtended;
