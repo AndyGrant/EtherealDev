c91f6cae18d605c1e90adbdadf5385014d314ec7
AndyGrant <Andrew@grantnet.us>
2018-06-24 14:25:49 -0400

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -643,7 +643,7 @@ int evaluateKings(EvalInfo* ei, Board* board, int colour){
 
         count += KSAttackValue     * scaledAttackCounts
                + KSWeakSquares     * popcount(weak & ei->kingAreas[US])
-               + KSFriendlyPawns   * popcount(myPawns & ei->kingAreas[US])
+               + KSFriendlyPawns   * popcount(~weak & myPawns & ei->kingAreas[US])
                + KSNoEnemyQueens   * !enemyQueens
                + KSSafeQueenCheck  * !!queenChecks
                + KSSafeRookCheck   * !!rookChecks
