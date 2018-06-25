df30305e0a27e6f043e22e7e8e974eb38b053449
AndyGrant <Andrew@grantnet.us>
2018-06-25 01:31:52 -0400

    .

--- src/search.c
+++ src/search.c
@@ -538,7 +538,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         if (   !PvNode
             &&  isQuiet
             &&  best > MATED_IN_MAX
-            && (hist < 4096 || !improving)
+            && (hist < 2048 || !improving)
             &&  futilityMargin <= alpha
             &&  depth <= FutilityPruningDepth)
             break;
