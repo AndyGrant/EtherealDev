d3887267d480c8e3004db05e90ccfd101720cd6a
AndyGrant <Andrew@grantnet.us>
2018-06-26 23:31:48 -0400

    .

--- src/search.c
+++ src/search.c
@@ -427,7 +427,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     if (   !PvNode
         && !inCheck
         &&  depth <= BetaPruningDepth
-        &&  eval - BetaMargin * depth > beta)
+        &&  eval - BetaMargin * depth > beta
+        &&  qsearch(thread, pv, alpha, beta, height) >= beta)
         return beta;
 
     // Step 9. Null Move Pruning. If our position is so good that
