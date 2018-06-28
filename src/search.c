6b9a48d5dbbec276c270a5673e207407f95605f9
AndyGrant <Andrew@grantnet.us>
2018-06-27 21:05:47 -0400

    .

--- src/search.c
+++ src/search.c
@@ -538,6 +538,13 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             &&  depth <= FutilityPruningDepth)
             break;
 
+        if (   !PvNode
+            &&  isQuiet
+            &&  best > MATED_IN_MAX
+            &&  depth <= 2
+            &&  getCMHistoryScore(thread, height, move) < -4000)
+            continue;
+
         // Step 14. Late Move Pruning / Move Count Pruning. If we have
         // tried many quiets in this position already, and we don't expect
         // anything from this move, we can undo it and skip all remaining quiets
