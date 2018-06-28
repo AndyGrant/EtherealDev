1e293a71983e5bb669fac3ef83492b30cccab53c
AndyGrant <Andrew@grantnet.us>
2018-06-27 23:26:39 -0400

    .

--- src/search.h
+++ src/search.h
@@ -90,7 +90,7 @@ static const int FutilityMargin = 100;
 
 static const int FutilityPruningDepth = 8;
 
-static const int CounterMovePruningDepth = 2;
+static const int CounterMovePruningDepth = 3;
 
 static const int LateMovePruningDepth = 8;
 
