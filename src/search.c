28500295951e5d9e9484359e28bb6356e22339be
AndyGrant <Andrew@grantnet.us>
2018-06-28 08:58:06 -0400

    9251466

--- src/search.c
+++ src/search.c
@@ -534,7 +534,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         if (   !RootNode
             &&  isQuiet
             &&  best > MATED_IN_MAX
-            && (hist < 6000 || !improving)
+            && (hist < 5000 || !improving)
             &&  futilityMargin <= alpha
             &&  depth <= FutilityPruningDepth)
             break;
@@ -598,7 +598,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
               || move == movePicker.counter;
 
             // Adjust based on history
-            R -= hist / 6000;
+            R -= hist / 5000;
 
             // Don't extend or drop into QS
             R  = MIN(depth - 1, MAX(R, 1));
