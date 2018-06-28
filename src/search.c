a56e02a0d9ecb37391b6a192ab068e131661a295
AndyGrant <Andrew@grantnet.us>
2018-06-28 00:55:41 -0400

    .

--- src/search.c
+++ src/search.c
@@ -552,6 +552,9 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         // Step 15. Counter Move Pruning. Moves with poor counter
         // move history are pruned at near leaf nodes of the search.
         if (   !PvNode
+            &&  move != movePicker.killer1
+            &&  move != movePicker.killer2
+            &&  move != movePicker.counter
             &&  isQuiet
             &&  best > MATED_IN_MAX
             &&  depth <= CounterMovePruningDepth
