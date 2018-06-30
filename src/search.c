3ce8d0339d6733442e6863d05d50bef478bdc949
AndyGrant <Andrew@grantnet.us>
2018-06-30 01:17:56 -0400

    .

--- src/search.c
+++ src/search.c
@@ -603,6 +603,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // Adjust based on history
             R -= MAX(-2, MIN(2, hist / 5000));
 
+            R += hist < -4096 && thread->histStack[height-2] < -4096;
+
             // Don't extend or drop into QS
             R  = MIN(depth - 1, MAX(R, 1));
 
