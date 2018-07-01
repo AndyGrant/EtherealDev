86a085fea97aa0a16b3091b2a797a8687d782c95
AndyGrant <Andrew@grantnet.us>
2018-07-01 04:15:23 -0400

    .

--- src/search.c
+++ src/search.c
@@ -442,7 +442,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         &&  hasNonPawnMaterial(board, board->turn)
         &&  board->history[board->numMoves-1] != NULL_MOVE){
 
-        R = 4 + depth / 6 + MIN(3, (eval - beta) / 200);
+        R = 4 + depth / 6 + MIN(3, (eval - beta + 100) / 200);
 
         applyNullMove(board, undo);
 
