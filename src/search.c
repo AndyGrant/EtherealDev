319c518a5ae2ca367de44117aa1fc41b401dd16c
AndyGrant <Andrew@grantnet.us>
2018-07-02 00:50:44 -0400

    .

--- src/search.c
+++ src/search.c
@@ -598,7 +598,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // Reduce for Killers and Counters
             R -= move == movePicker.killer1
               || move == movePicker.killer2
-              || move == movePicker.counter;
+              || move == movePicker.counter
+              || quiets < 4;
 
             // Adjust based on history
             R -= MAX(-2, MIN(2, hist / 5000));
