62f34e652ac51306410c3258f0b4ed0a6dc4c00e
AndyGrant <Andrew@grantnet.us>
2018-07-04 11:51:21 -0400

    .

--- src/search.c
+++ src/search.c
@@ -203,19 +203,15 @@ int aspirationWindow(Thread* thread, int depth){
     const int mainThread = thread == &thread->threads[0];
     const int aspDepth   = thread->info->depth;
 
-    int alpha, beta, value, upper, lower;
+    int alpha, beta, value, delta = 10;
 
     // Without at least a few searches, we cannot guess a good search window
     if (depth <= 4 || aspDepth <= 4)
         return search(thread, &thread->pv, -MATE, MATE, depth, 0);
 
-    // Compute bounds based on score difference of last iteration
-    upper = MAX(12, abs(values[aspDepth] - values[aspDepth-1]));
-    lower = MAX(12, abs(values[aspDepth] - values[aspDepth-1]));
-
     // Create the aspiration window
-    alpha = MAX(-MATE, values[aspDepth] - lower);
-    beta  = MIN( MATE, values[aspDepth] + upper);
+    alpha = MAX(-MATE, values[aspDepth] - delta);
+    beta  = MIN( MATE, values[aspDepth] + delta);
 
     // Keep trying larger windows until one works
     while (1) {
@@ -233,15 +229,14 @@ int aspirationWindow(Thread* thread, int depth){
         // Search failed low
         if (value <= alpha) {
             beta  = (alpha + beta) / 2;
-            alpha = MAX(-MATE, alpha - lower);
-            lower = lower + lower / 2;
+            alpha = MAX(-MATE, alpha - delta);
         }
 
         // Search failed high
-        if (value >= beta) {
-            beta  = MIN(MATE, beta + upper);
-            upper = upper + upper / 2;
-        }
+        if (value >= beta)
+            beta = MIN(MATE, beta + delta);
+
+        delta = delta + delta / 2;
     }
 }
 
