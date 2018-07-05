2fb21d2f3a09454f1e7f2e35dc2907b05da1ccbb
AndyGrant <Andrew@grantnet.us>
2018-07-04 21:01:20 -0400

    .

--- src/search.c
+++ src/search.c
@@ -533,7 +533,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         if (   !RootNode
             &&  isQuiet
             &&  best > MATED_IN_MAX
-            && (hist < 6000 || !improving)
+            && (hist < 12000 || !improving)
             &&  futilityMargin <= alpha
             &&  depth <= FutilityPruningDepth){
             skipQuiets = 1;
