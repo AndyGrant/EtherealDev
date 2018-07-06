ced339278c80fffc204a4b08aa04baadf149a11d
AndyGrant <Andrew@grantnet.us>
2018-07-06 00:40:32 -0400

    .

--- src/search.c
+++ src/search.c
@@ -609,8 +609,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         // Step 18B. Check Extensions. We extend captures and good quiets that
         // come from in check positions, so long as no other extensions occur
         extension += !RootNode
-                  &&  inCheck
-                  && !extension;
+                  && !extension
+                  &&  board->kingAttackers;
 
         // New depth is what our search depth would be, assuming that we do no LMR
         newDepth = depth + extension;
