87a613663e0c9d7bbb7b4efb9c4b0c7a829e9b31
AndyGrant <Andrew@grantnet.us>
2018-07-14 00:24:34 -0400

    .

--- src/search.h
+++ src/search.h
@@ -96,7 +96,7 @@ static const int LateMovePruningCounts[2][9] = {
 };
 
 static const int SEEPruningDepth = 8;
-static const int SEEMargin = -20;
+static const int SEEMargin = -18;
 
 static const int QFutilityMargin = 100;
 
