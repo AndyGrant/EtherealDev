35860bad40c51e1584fd313670e41a080d568018
AndyGrant <Andrew@grantnet.us>
2018-07-17 02:45:59 -0400

    .

--- src/search.c
+++ src/search.c
@@ -584,7 +584,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         extension += !RootNode
                   &&  inCheck
                   && !extension
-                  &&  movePicker.stage > STAGE_BAD_NOISY;
+                  &&  movePicker.stage < STAGE_BAD_NOISY;
 
         // New depth is what our search depth would be, assuming that we do no LMR
         newDepth = depth + extension;
