01942554d0355d2e00baa1ff5ea5e8e9128a6278
AndyGrant <Andrew@grantnet.us>
2018-07-19 10:16:57 -0400

    .

--- src/search.c
+++ src/search.c
@@ -589,7 +589,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         // and it seems that under some conditions, the table move is better than
         // all other possible moves, we will extend the search of the table move
         extension += !RootNode
-                  && !extension;
+                  && !extension
                   &&  depth >= 10
                   &&  move == ttMove
                   &&  ttDepth >= depth - 3
