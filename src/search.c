5712dd1566ae72857705807c69983bdda8e81ba3
AndyGrant <Andrew@grantnet.us>
2018-07-22 20:33:10 -0400

    .

--- src/search.c
+++ src/search.c
@@ -520,10 +520,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             &&  isQuiet
             &&  best > MATED_IN_MAX
             &&  depth <= LateMovePruningDepth
-            &&  quiets > LateMovePruningCounts[improving][depth]){
+            &&  quiets > LateMovePruningCounts[improving][depth])
             skipQuiets = 1;
-            continue;
-        }
 
         // Step 15. Counter Move Pruning. Moves with poor counter
         // move history are pruned at near leaf nodes of the search.
