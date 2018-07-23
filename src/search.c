4cea507ec0bd439d56ca32c23d7c8b1a47db6e6f
AndyGrant <Andrew@grantnet.us>
2018-07-22 22:56:24 -0400

    .

--- src/search.c
+++ src/search.c
@@ -520,6 +520,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             &&  isQuiet
             &&  best > MATED_IN_MAX
             &&  depth <= LateMovePruningDepth
+            &&  hist < LateMovePruningHistoryLimit[improving]
             &&  quiets > LateMovePruningCounts[improving][depth]){
             skipQuiets = 1;
             continue;
--- src/search.h
+++ src/search.h
@@ -94,6 +94,7 @@ static const int FollowUpMovePruningDepth = 2;
 static const int FollowUpMoveHistoryLimit[] = { -2000, -4000 };
 
 static const int LateMovePruningDepth = 8;
+static const int LateMovePruningHistoryLimit[] = { 12000, 6000 };
 static const int LateMovePruningCounts[2][9] = {
     {  0,  3,  4,  7, 12, 16, 21, 28, 34},
     {  0,  5,  7, 12, 18, 27, 38, 50, 65},
