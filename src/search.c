f79d6fdb3e1b55587db18fdeb702be83c01ccb3d
AndyGrant <Andrew@grantnet.us>
2018-07-24 10:22:42 -0400

    .

--- src/search.c
+++ src/search.c
@@ -388,7 +388,9 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     if (   !PvNode
         && !inCheck
         &&  depth <= RazorDepth
-        &&  eval + RazorMargin < alpha)
+        &&  eval + RazorMargin < alpha
+        &&  hasNonPawnMaterial(board, WHITE)
+        &&  hasNonPawnMaterial(board, BLACK))
         return qsearch(thread, pv, alpha, beta, height);
 
     // Step 8. Beta Pruning / Reverse Futility Pruning / Static Null
