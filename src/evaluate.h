4b32615a9bd62071a705625f309ff92daeed862d
AndyGrant <Andrew@grantnet.us>
2018-07-24 10:13:20 -0400

    .

--- src/evaluate.h
+++ src/evaluate.h
@@ -22,7 +22,7 @@
 #include "types.h"
 
 enum {
-    SCALE_OCB_BISHOPS_ONLY =  64,
+    SCALE_OCB_BISHOPS_ONLY =  80,
     SCALE_OCB_ONE_KNIGHT   = 106,
     SCALE_OCB_ONE_ROOK     =  96,
     SCALE_OCB_TWO_ROOKS    = 108,
