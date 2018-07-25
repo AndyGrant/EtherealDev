b53ed775e660db89d292e1490a93b7c13120d796
AndyGrant <Andrew@grantnet.us>
2018-07-25 00:55:25 -0400

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -60,11 +60,11 @@ const int PieceValues[8][PHASE_NB] = {
 
 /* Pawn Evaluation Terms */
 
-const int PawnIsolated = S(  -3,  -1);
+const int PawnIsolated = S(  -6,   1);
 
-const int PawnStacked = S( -10, -34);
+const int PawnStacked = S( -10, -32);
 
-const int PawnBackwards[2] = { S(   7,  -2), S( -10, -13) };
+const int PawnBackwards[2] = { S(   8,  -4), S( -14, -13) };
 
 const int PawnConnected32[32] = {
     S(   0,   0), S(   0,   0), S(   0,   0), S(   0,   0),
