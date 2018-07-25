b42695cafb51c0a2098c397e6eeb917e41181d05
AndyGrant <Andrew@grantnet.us>
2018-07-25 01:53:58 -0400

    .

--- src/search.c
+++ src/search.c
@@ -515,7 +515,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             &&  isQuiet
             &&  best > MATED_IN_MAX
             &&  depth <= LateMovePruningDepth
-            &&  quiets > LateMovePruningCounts[improving || PvNode][depth]){
+            &&  quiets > LateMovePruningCounts[improving][depth]){
             skipQuiets = 1;
             continue;
         }
