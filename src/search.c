a2b470d06892e430788af51789d697ee31b83ed2
AndyGrant <Andrew@grantnet.us>
2018-07-25 00:50:31 -0400

    .

--- src/search.c
+++ src/search.c
@@ -427,7 +427,6 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     // likely going to be good at a full depth. To save some work we will prune
     // captures that won't exceed rbeta or captures that fail at a low depth
     if (   !PvNode
-        && !inCheck
         &&  abs(beta) < MATE_IN_MAX
         &&  depth >= ProbCutDepth
         &&  eval + bestTacticalMoveValue(board) >= beta + ProbCutMargin){
@@ -452,11 +451,11 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             thread->moveStack[height] = move;
             thread->pieceStack[height] = pieceType(board->squares[MoveTo(move)]);
 
-            // Verify the move is good with a depth zero search (qsearch, unless in check)
-            // and then with a slightly reduced search. If both searches still exceed rBeta,
-            // we will prune this node's subtree with resonable assurance that we made no error
-            value = -search(thread, &lpv, -rBeta, -rBeta+1, 0, height+1);
-            if (value >= rBeta)
+            // Verify the move has promise using a depth 1 search
+            value = -search(thread, &lpv, -rBeta, -rBeta+1, 1, height+1);
+
+            // Verify the move holds which a slightly reduced depth search
+            if (value >= rBeta && depth > 5)
                 value = -search(thread, &lpv, -rBeta, -rBeta+1, depth-4, height+1);
 
             // Revert the board state
