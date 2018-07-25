d26778fdd9bb2a20e4c084f28d59f56efe200a2c
AndyGrant <Andrew@grantnet.us>
2018-07-25 02:27:05 -0400

    .

--- src/search.c
+++ src/search.c
@@ -407,7 +407,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         &&  thread->moveStack[height-1] != NULL_MOVE
         && (!ttHit || !(ttBound & BOUND_UPPER) || ttValue >= beta)) {
 
-        R = 4 + depth / 6 + MIN(2, (eval - beta) / 300);
+        R = 4 + depth / 6 + MIN(4, (eval - beta) / 150);
 
         applyNullMove(board, undo);
 
