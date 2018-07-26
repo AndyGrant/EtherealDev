1ffbe19f46be0e075dbe3fa3d119b73498dc0d75
AndyGrant <Andrew@grantnet.us>
2018-07-26 02:15:42 -0400

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -60,9 +60,9 @@ const int PieceValues[8][PHASE_NB] = {
 
 /* Pawn Evaluation Terms */
 
-const int PawnIsolated = S(  -3,  -1);
-
 const int PawnStacked = S( -10, -34);
+
+const int PawnIsolated = S(  -7,  -4);
 
 const int PawnBackwards[2] = { S(   7,  -2), S( -10, -13) };
 
@@ -329,16 +329,16 @@ int evaluatePawns(EvalInfo *ei, Board *board, int colour) {
         if (!(passedPawnMasks(US, sq) & enemyPawns))
             setBit(&ei->passedPawns, sq);
 
-        // Apply a penalty if the pawn is isolated
-        if (!(isolatedPawnMasks(sq) & myPawns)) {
-            pkeval += PawnIsolated;
-            if (TRACE) T.PawnIsolated[US]++;
-        }
-
         // Apply a penalty if the pawn is stacked
         if (Files[fileOf(sq)] & tempPawns) {
             pkeval += PawnStacked;
             if (TRACE) T.PawnStacked[US]++;
+        }
+
+        // Apply a penalty if the pawn is isolated
+        else if (!(isolatedPawnMasks(sq) & myPawns)) {
+            pkeval += PawnIsolated;
+            if (TRACE) T.PawnIsolated[US]++;
         }
 
         // Apply a penalty if the pawn is backward
