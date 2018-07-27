d44134e959bd8ca0a8fc4e64e7f5da46ccc22fd1
AndyGrant <Andrew@grantnet.us>
2018-07-27 00:54:53 -0400

    .

--- src/search.c
+++ src/search.c
@@ -413,7 +413,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             value = -search(thread, &lpv, -rBeta, -rBeta+1, 2, height+1);
 
             // Verify the move holds which a slightly reduced depth search
-            if (value >= rBeta && depth > 6)
+            if (value >= rBeta)
                 value = -search(thread, &lpv, -rBeta, -rBeta+1, depth-4, height+1);
 
             // Revert the board state
