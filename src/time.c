63e4927e44abbdee30e62272f56e7ade31b1b6d6
AndyGrant <Andrew@grantnet.us>
2018-07-27 09:29:41 -0400

    .

--- src/time.c
+++ src/time.c
@@ -106,11 +106,6 @@ void updateTimeManagment(SearchInfo* info, Limits* limits, int depth, int value)
     if (info->values[depth-1] > value + 20)
         info->idealUsage *= 1.050;
 
-    // Increase our time if the score suddenly dropped
-    if (info->values[depth-1] > value + 40)
-        info->idealUsage *= 1.050;
-
-
     if (info->bestMoves[depth] == info->bestMoves[depth-1]){
 
         // If we still have remaining increments from best move
