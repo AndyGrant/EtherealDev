132ee138e181de6c0ccd4033095fed77ae148d57
AndyGrant <Andrew@grantnet.us>
2018-07-30 02:46:05 -0400

    .

--- src/search.c
+++ src/search.c
@@ -531,7 +531,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // Increase for non improving nodes
             R += !improving;
 
-            R += !PvNode && inCheck && cmhist < 0 && hist < 0;
+            R -= inCheck && cmhist > 0 && hist > 0;
 
             // Reduce for Killers and Counters
             R -= move == movePicker.killer1
