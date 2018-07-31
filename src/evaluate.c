4fa43effd1327a37918bba24b8d8f91bda6105fa
AndyGrant <Andrew@grantnet.us>
2018-07-31 09:47:38 -0400

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -188,7 +188,7 @@ const int PassedFriendlyDistance = S(   2,  -6);
 
 const int PassedEnemyDistance = S(  -1,   8);
 
-const int PassedConnectedPassers = S(   5,  10);
+const int PassedConnectedPassers = S(  10,  20);
 
 /* Threat Evaluation Terms */
 
