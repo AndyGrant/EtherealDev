61c7ddcc664e10dc571e420ff862c3db4561e77c
AndyGrant <Andrew@grantnet.us>
2018-08-01 01:35:37 -0400

    .

--- src/texel.h
+++ src/texel.h
@@ -27,7 +27,7 @@
 #define NPARTITIONS (     64) // # of partitions to use
 
 #define NDEPTHS     (      0) // # of search iterations
-#define NTERMS      (      0) // # of terms to tune
+#define NTERMS      (     32) // # of terms to tune
 #define NPOSITIONS  (1491000) // # of FENs in book
 
 // Each Eval Term (Total = 490)
@@ -46,7 +46,7 @@
 #define TunePawnIsolated                (0)
 #define TunePawnStacked                 (0)
 #define TunePawnBackwards               (0)
-#define TunePawnConnected32             (0)
+#define TunePawnConnected32             (1)
 #define TuneKnightOutpost               (0)
 #define TuneKnightBehindPawn            (0)
 #define TuneKnightMobility              (0)
