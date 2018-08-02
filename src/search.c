d43b58a5323ed6b7b5e3830d0e6a39ba74f8de3d
AndyGrant <Andrew@grantnet.us>
2018-08-02 02:05:18 -0400

    .

--- src/search.c
+++ src/search.c
@@ -483,6 +483,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         if (   !RootNode
             &&  isQuiet
             &&  best > MATED_IN_MAX
+            &&  move != movePicker.counter
             &&  depth <= CounterMovePruningDepth
             &&  cmhist < CounterMoveHistoryLimit[improving])
             continue;
@@ -492,6 +493,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         if (   !RootNode
             &&  isQuiet
             &&  best > MATED_IN_MAX
+            &&  move != movePicker.counter
             &&  depth <= FollowUpMovePruningDepth
             &&  fuhist < FollowUpMoveHistoryLimit[improving])
             continue;
