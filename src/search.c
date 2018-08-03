40d62ef17cbf40e2d55cf6361b7a975162c21ad6
AndyGrant <Andrew@grantnet.us>
2018-08-03 01:23:54 -0400

    .

--- src/search.c
+++ src/search.c
@@ -532,6 +532,9 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // Increase for non improving nodes
             R += !improving;
 
+            // Decrease for strong checking moves
+            R -= hist > 0 && board->kingAttackers;
+
             // Reduce for Killers and Counters
             R -= move == movePicker.killer1
               || move == movePicker.killer2
@@ -557,9 +560,10 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
 
         // Step 19B. Check Extensions. We extend captures and good quiets that
         // come from in check positions, so long as no other extensions occur
-        extension += !RootNode
-                  &&  inCheck
-                  && !extension;
+        extension +=  !RootNode
+                  &&  !extension
+                  &&   inCheck
+                  && (!isQuiet || hist > 0);
 
         // New depth is what our search depth would be, assuming that we do no LMR
         newDepth = depth + extension;
