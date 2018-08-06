c7e6b47f2ac4b6d0835661aa3944bac95f7b40fa
AndyGrant <Andrew@grantnet.us>
2018-08-06 02:00:17 -0400

    .

--- src/search.h
+++ src/search.h
@@ -84,7 +84,7 @@ static const int ProbCutMargin = 100;
 static const int IIDDepth = 3;
 
 static const int FutilityMargin = 100;
-static const int FutilityPruningDepth = 8;
+static const int FutilityPruningDepth = 6;
 static const int FutilityPruningHistoryLimit[] = { 12000, 6000 };
 
 static const int CounterMovePruningDepth[] = { 3, 2 };
