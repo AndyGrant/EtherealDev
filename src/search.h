27ae4107fc90d3368d2ef4c5c3985a27a57e0163
AndyGrant <Andrew@grantnet.us>
2018-08-08 02:07:31 -0400

    .

--- src/search.h
+++ src/search.h
@@ -74,7 +74,8 @@ static const int RazorDepth = 1;
 static const int RazorMargin = 350;
 
 static const int BetaPruningDepth = 8;
-static const int BetaMargin = 85;
+static const int BetaMargin[] = { 0, 85, 170, 260, 350, 445, 550, 675, 840 };
+
 
 static const int NullMovePruningDepth = 2;
 
