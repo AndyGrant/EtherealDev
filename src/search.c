9b49fadc429f6697fad0a25ab5229e0d37373b56
AndyGrant <Andrew@grantnet.us>
2018-08-08 23:39:54 -0400

    .

--- src/search.c
+++ src/search.c
@@ -392,13 +392,9 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
 
         rBeta = MIN(beta + ProbCutMargin, MATE - MAX_PLY - 1);
 
-        initMovePicker(&movePicker, thread, NONE_MOVE, height);
+        initNoisyMovePicker(&movePicker, thread, rBeta - eval);
 
         while ((move = selectNextMove(&movePicker, board, 1)) != NONE_MOVE){
-
-            // Move should pass an SEE() to be worth at least rBeta
-            if (!staticExchangeEvaluation(board, move, rBeta - eval))
-                continue;
 
             // Apply and validate move before searching
             applyMove(board, move, undo);
