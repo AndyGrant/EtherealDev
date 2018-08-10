cd1b53b8f6f3dccc4f7690d6f015c82aa8ae1163
AndyGrant <Andrew@grantnet.us>
2018-08-10 02:54:49 -0400

    .

--- src/texel.h
+++ src/texel.h
@@ -27,7 +27,7 @@
 #define NPARTITIONS (     64) // # of partitions to use
 
 #define NDEPTHS     (      0) // # of search iterations
-#define NTERMS      (      3) // # of terms to tune
+#define NTERMS      (    131) // # of terms to tune
 #define NPOSITIONS  (1491000) // # of FENs in book
 
 // Each Eval Term (Total = 490)
@@ -60,7 +60,7 @@
 #define TuneRookMobility                (0)
 #define TuneQueenMobility               (0)
 #define TuneKingDefenders               (1)
-#define TuneKingShelter                 (0)
+#define TuneKingShelter                 (1)
 #define TunePassedPawn                  (0)
 #define TunePassedFriendlyDistance      (0)
 #define TunePassedEnemyDistance         (0)
