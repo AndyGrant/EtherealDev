2abb1302395b7ca4d7af6c7b2593d9e4faaeaf2c
AndyGrant <Andrew@grantnet.us>
2018-08-11 01:05:15 -0400

    .

--- src/search.c
+++ src/search.c
@@ -117,7 +117,7 @@ void* iterativeDeepening(void* vthread){
                 count += thread != &thread->threads[i]
                       && thread->threads[i].depth >= depth;
 
-            if (count >= thread->nthreads / 3){
+            if (depth <= info->depth || count >= thread->nthreads / 2){
                 thread->depth = depth + 1;
                 pthread_mutex_unlock(&LOCK);
                 continue;
