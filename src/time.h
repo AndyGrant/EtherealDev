236944bbaf51256325723ce2e8a0bc53d9b382ca
AndyGrant <Andrew@grantnet.us>
2018-08-13 23:47:50 -0400

    .

--- src/time.h
+++ src/time.h
@@ -29,7 +29,7 @@ int terminateTimeManagment(SearchInfo* info);
 
 static const double ScoreAdjustWeight = 0.050;
 
-static const double PVAdjustCount = 5;
-static const double PVAdjustWeight = 0.100;
+static const double PVAdjustCount = 7;
+static const double PVAdjustWeight = 0.080;
 
 #endif
