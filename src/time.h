4452e94998b3a1f407b167beec69c04eb4904b60
AndyGrant <Andrew@grantnet.us>
2018-08-17 20:35:22 -0400

    .

--- src/time.h
+++ src/time.h
@@ -27,7 +27,7 @@ void initTimeManagment(SearchInfo* info, Limits* limits);
 void updateTimeManagment(SearchInfo* info, Limits* limits, int depth, int value);
 int terminateTimeManagment(SearchInfo* info);
 
-static const double PVFactorCount  = 6;
-static const double PVFactorWeight = 0.08;
+static const double PVFactorCount  = 8;
+static const double PVFactorWeight = 0.07;
 
 #endif
