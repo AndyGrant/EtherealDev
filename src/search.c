c27eab99387581d42e9c96762225ec7c4db2c514
AndyGrant <Andrew@grantnet.us>
2018-08-24 03:36:26 -0400

    .

--- src/search.c
+++ src/search.c
@@ -540,9 +540,10 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             R += !improving;
 
             // Reduce for Killers and Counters
-            R -= move == movePicker.killer1
-              || move == movePicker.killer2
-              || move == movePicker.counter;
+            if (ttMove == NONE_MOVE)
+                R -= move == movePicker.killer1
+                || move == movePicker.killer2
+                || move == movePicker.counter;
 
             // Adjust based on history
             R -= MAX(-2, MIN(2, hist / 5000));
