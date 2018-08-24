88993ce782fafeed0d599a27b26d589120f34651
AndyGrant <Andrew@grantnet.us>
2018-08-23 23:58:59 -0400

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -165,11 +165,11 @@ const int KSAttackValue      =   44;
 const int KSWeakSquares      =   38;
 const int KSFriendlyPawns    =  -22;
 const int KSNoEnemyQueens    = -276;
-const int KSSafeQueenCheck   =   95;
-const int KSSafeRookCheck    =   94;
-const int KSSafeBishopCheck  =   51;
-const int KSSafeKnightCheck  =  123;
-const int KSSafeCheckSupport =   20;
+const int KSSafeQueenCheck   =   90;
+const int KSSafeRookCheck    =   90;
+const int KSSafeBishopCheck  =   50;
+const int KSSafeKnightCheck  =  120;
+const int KSSafeCheckSupport =   30;
 const int KSAdjustment       =  -18;
 
 /* Passed Pawn Evaluation Terms */
