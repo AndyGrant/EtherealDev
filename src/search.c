0c373f24534ff5f3ed4efae4cc20f1f6b9d586f9
AndyGrant <Andrew@grantnet.us>
2018-08-24 03:28:09 -0400

    .

--- src/search.c
+++ src/search.c
@@ -558,7 +558,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         extension =  !RootNode
                   &&  depth >= 10
                   &&  move == ttMove
-                  &&  ttDepth >= depth - 3
+                  &&  ttDepth >= depth - (PvNode ? 2 : 4)
                   && (ttBound & BOUND_LOWER)
                   &&  moveIsSingular(thread, ttMove, ttValue, undo, depth, height);
 
