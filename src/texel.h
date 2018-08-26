b19884f8607d1711133082b740cf8dda2bb283c9
AndyGrant <Andrew@grantnet.us>
2018-08-26 02:58:33 -0400

    .

--- src/texel.h
+++ src/texel.h
@@ -25,12 +25,12 @@
 #define CLEARING    (      1) // Clear hashes between runs
 #define RESOLVE     (      1) // Resolve with qsearch
 #define NPARTITIONS (     64) // Total thread partitions
-#define LEARNING    (    0.1) // Learning rate step size
+#define LEARNING    (   10.0) // Learning rate step size
 #define REPORTING   (    100) // How often to report progress
 
 #define NDEPTHS     (      0) // # of search iterations
-#define NTERMS      (      0) // # of terms to tune
-#define NPOSITIONS  (1491723) // # of FENs in book
+#define NTERMS      (     64) // # of terms to tune
+#define NPOSITIONS  (1364312) // # of FENs in book
 
 #define TunePawnValue                   (0)
 #define TuneKnightValue                 (0)
@@ -62,7 +62,7 @@
 #define TuneQueenMobility               (0)
 #define TuneKingDefenders               (0)
 #define TuneKingShelter                 (0)
-#define TuneKingStorm                   (0)
+#define TuneKingStorm                   (1)
 #define TunePassedPawn                  (0)
 #define TunePassedFriendlyDistance      (0)
 #define TunePassedEnemyDistance         (0)
