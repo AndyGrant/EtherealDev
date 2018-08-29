3a0e78c64d1057b7f958fd6e77420532f041045f
AndyGrant <Andrew@grantnet.us>
2018-08-29 06:51:56 -0400

    .

--- src/search.c
+++ src/search.c
@@ -574,7 +574,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         extension += !RootNode
                   && !extension
                   &&  inCheck
-                  &&  played < 10;
+                  &&  played < 5;
 
         // New depth is what our search depth would be, assuming that we do no LMR
         newDepth = depth + extension;
