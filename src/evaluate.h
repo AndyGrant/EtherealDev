15e69313cd2615360cd756e41efeda00ca15e50b
AndyGrant <Andrew@grantnet.us>
2018-08-29 22:55:47 -0400

    .

--- src/evaluate.h
+++ src/evaluate.h
@@ -23,8 +23,8 @@
 
 enum {
     SCALE_OCB_BISHOPS_ONLY =  64,
-    SCALE_OCB_ONE_KNIGHT   = 106,
-    SCALE_OCB_ONE_ROOK     =  96,
+    SCALE_OCB_ONE_KNIGHT   =  96,
+    SCALE_OCB_ONE_ROOK     =  86,
     SCALE_NORMAL           = 128,
 };
 
