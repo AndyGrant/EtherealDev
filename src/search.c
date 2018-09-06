03d0757f810cf613a96e56a7320088fd7ed24825
AndyGrant <Andrew@grantnet.us>
2018-09-06 01:52:56 -0400

    .

--- src/search.c
+++ src/search.c
@@ -547,9 +547,9 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             R += !improving;
 
 
-            R -= (cmhist < -5000) && (fuhist >  5000);
+            R -= (cmhist < -7500) && (fuhist >  7500);
 
-            R -= (cmhist >  5000) && (fuhist < -5000);
+            R -= (cmhist >  7500) && (fuhist < -7500);
 
             // Reduce for Killers and Counters
             R -= move == movePicker.killer1
