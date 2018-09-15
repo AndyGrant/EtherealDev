6d4564957d93f8930a9c6464a60b1b79440eb6e9
AndyGrant <Andrew@grantnet.us>
2018-09-15 02:04:02 -0400

    .

--- src/time.c
+++ src/time.c
@@ -97,28 +97,24 @@ void updateTimeManagment(SearchInfo* info, Limits* limits, int depth, int value)
 
     // Don't adjust time when we are at low depths, or if
     // we simply are not in control of our own time usage
-    if (!limits->limitedBySelf || depth < 4)
+    if (!limits->limitedBySelf || depth < 8)
         return;
 
     // Increase our time if the score suddenly dropped
-    if (lastValue > value + 10)
-        info->idealUsage *= 1.050;
-
-    // Increase our time if the score suddenly dropped
     if (lastValue > value + 20)
-        info->idealUsage *= 1.050;
+        info->idealUsage *= 1.075;
 
     // Increase our time if the score suddenly dropped
     if (lastValue > value + 40)
-        info->idealUsage *= 1.050;
+        info->idealUsage *= 1.075;
 
     // Increase our time if the score suddenly jumps
-    if (lastValue + 15 < value)
-        info->idealUsage *= 1.025;
+    if (lastValue + 20 < value)
+        info->idealUsage *= 1.075;
 
     // Increase our time if the score suddenly jumps
-    if (lastValue + 30 < value)
-        info->idealUsage *= 1.050;
+    if (lastValue + 40 < value)
+        info->idealUsage *= 1.075;
 
     // Always scale back the PV time factor
     info->pvFactor = MAX(0, info->pvFactor - 1);
