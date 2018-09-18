18e8e6ab2df2b58e957ee7e33351db02c3b78a21
AndyGrant <Andrew@grantnet.us>
2018-09-18 01:19:48 -0400

    .

--- src/time.c
+++ src/time.c
@@ -70,9 +70,9 @@ void initTimeManagment(SearchInfo* info, Limits* limits){
 
         // Playing using X + Y or X time controls
         else {
-            info->idealUsage =  0.52 * (limits->time + 23 * limits->inc) / 25;
-            info->maxAlloc   =  4.00 * (limits->time + 23 * limits->inc) / 25;
-            info->maxUsage   = 10.00 * (limits->time + 23 * limits->inc) / 25;
+            info->idealUsage =  0.50 * (limits->time + 25 * limits->inc) / 30;
+            info->maxAlloc   =  4.00 * (limits->time + 25 * limits->inc) / 30;
+            info->maxUsage   = 10.00 * (limits->time + 25 * limits->inc) / 30;
         }
 
         // Cap all time allocations using the move time buffer
