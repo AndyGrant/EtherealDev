49cc9476914000777210edf9f239e2c204069a6b
AndyGrant <Andrew@grantnet.us>
2018-09-18 22:09:45 -0400

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -203,7 +203,7 @@ const int KingStorm[2][FILE_NB/2][RANK_NB] = {
 /* King Safety Evaluation Terms */
 
 const int KSAttackWeight[]  = { 0, 16, 6, 10, 8, 0 };
-const int KSAttackValue     =   50;
+const int KSAttackValue     =   38;
 const int KSWeakSquares     =   38;
 const int KSFriendlyPawns   =  -22;
 const int KSNoEnemyQueens   = -276;
