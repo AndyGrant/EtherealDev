a3fb23c1faf93457009d82ddcbac0a4350f344cf
AndyGrant <Andrew@grantnet.us>
2018-09-20 09:13:57 -0400

    .

--- src/search.c
+++ src/search.c
@@ -552,7 +552,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
               || move == movePicker.counter;
 
             // Adjust based on history
-            R -= MAX(-2, MIN(2, hist / 7500));
+            R -= hist / 7500;
 
             // Don't extend or drop into QS
             R  = MIN(depth - 1, MAX(R, 1));
