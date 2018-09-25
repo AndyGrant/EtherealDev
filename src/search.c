fc9f761a91aedf34f6999b6aeb0632b24273f34b
AndyGrant <Andrew@grantnet.us>
2018-09-25 17:10:33 -0400

    .

--- src/search.c
+++ src/search.c
@@ -525,6 +525,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // Increase for non improving nodes
             R += !improving;
 
+            R -= fuhist > 5000 && thread->moveStack[height-1] == NULL_MOVE;
+
             // Reduce for Killers and Counters
             R -= move == movePicker.killer1
               || move == movePicker.killer2
