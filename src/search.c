bd805c519defd418b31da0805d86f4993ce15d8e
AndyGrant <Andrew@grantnet.us>
2018-11-18 19:58:08 -0500

    .

--- src/search.c
+++ src/search.c
@@ -58,8 +58,8 @@ void initSearch(){
     // Init Late Move Reductions Table
     for (int d = 1; d < 64; d++) {
         for (int p = 1; p < 64; p++) {
-            LMRTable[d][p][0] = 0.75 + log(d) * log(p * 1.10) / 2.25;
-            LMRTable[d][p][1] = 0.75 + log(d) * log(p * 0.80) / 2.25;
+            LMRTable[d][p][0] = 0.75 + log(d) * log(p) / 2.25;
+            LMRTable[d][p][1] = 0.25 + log(d) * log(p) / 2.25;
         }
     }
 }
@@ -551,13 +551,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
                   && (ttBound & BOUND_LOWER)
                   &&  moveIsSingular(thread, ttMove, ttValue, undo, depth, height);
 
-        // Step 19B. Check Extensions. We extend captures and good quiets that
-        // come from in check positions, so long as no other extensions occur
-        extension += !RootNode
-                  &&  inCheck
-                  && !extension;
-
-        // Step 19C. History Extensions. We extend quiet moves with strong
+        // Step 19B. History Extensions. We extend quiet moves with strong
         // history scores for both counter move and followups. We only apply
         // this extension to the first quiet moves tried during the search
         extension += !RootNode
