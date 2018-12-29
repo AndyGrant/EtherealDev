4ff379cabcbe42a67b3e9e4805db0056795164c8
AndyGrant <Andrew@grantnet.us>
2018-12-28 19:42:42 -0500

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -205,13 +205,14 @@ const int KingStorm[2][FILE_NB/2][RANK_NB] = {
 const int KSAttackWeight[]  = { 0, 16, 6, 10, 8, 0 };
 const int KSAttackValue     =   44;
 const int KSWeakSquares     =   38;
-const int KSStrongSquares   =  -35;
+const int KSStrongSquares   =  -10;
+const int KSFriendlyPawns   =  -20;
 const int KSNoEnemyQueens   = -276;
 const int KSSafeQueenCheck  =   95;
 const int KSSafeRookCheck   =   94;
 const int KSSafeBishopCheck =   51;
 const int KSSafeKnightCheck =  123;
-const int KSAdjustment      =  -18;
+const int KSAdjustment      =    0;
 
 /* Passed Pawn Evaluation Terms */
 
@@ -697,6 +698,7 @@ int evaluateKings(EvalInfo *ei, Board *board, int colour) {
         count += KSAttackValue     * scaledAttackCounts
                + KSWeakSquares     * popcount(weak & ei->kingAreas[US])
                + KSStrongSquares   * popcount(ei->pawnAttacks2[US] & ei->kingAreas[US])
+               + KSFriendlyPawns   * popcount(myPawns & ei->kingAreas[US] & ~weak)
                + KSNoEnemyQueens   * !enemyQueens
                + KSSafeQueenCheck  * popcount(queenChecks)
                + KSSafeRookCheck   * popcount(rookChecks)
