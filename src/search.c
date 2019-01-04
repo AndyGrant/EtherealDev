e5c1acbbbca93c53a2f6e42d2c9f6e43426492de
AndyGrant <Andrew@grantnet.us>
2019-01-03 21:01:42 -0500

    .

--- src/search.c
+++ src/search.c
@@ -529,7 +529,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // Increase for non improving nodes
             R += !improving;
 
-            R -= ttTactical && singularBreaker;
+            R -= ttTactical && move == singularBreaker;
 
             // Reduce for Killers and Counters
             R -= move == movePicker.killer1
