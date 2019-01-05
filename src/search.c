ecdcb64abb5794ed8218b8b6b3f833b112ea77be
AndyGrant <Andrew@grantnet.us>
2019-01-05 03:07:58 -0500

    .

--- src/search.c
+++ src/search.c
@@ -559,7 +559,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         // this extension to the first quiet moves tried during the search
         extension += !RootNode
                   && !extension
-                  &&  quiets <= 4
+                  &&  isQuiet
+                  &&  quiets == 1
                   &&  cmhist >= 10000
                   &&  fuhist >= 10000;
 
