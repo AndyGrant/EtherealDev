e76fd213c0ba5e6d6d801b9d509451f26ffa10c7
AndyGrant <Andrew@grantnet.us>
2019-01-04 19:41:06 -0500

    .

--- src/search.h
+++ src/search.h
@@ -74,7 +74,7 @@ static const int SkipSize[16]   = { 1, 1, 1, 2, 2, 2, 1, 3, 2, 2, 1, 3, 3, 2, 2,
 static const int SkipDepths[16] = { 1, 2, 2, 4, 4, 3, 2, 5, 4, 3, 2, 6, 5, 4, 3, 2 };
 
 static const int AspirationCycles     = 4;
-static const int AspirationWindows[4] = {14, 12, 16, 18};
+static const int AspirationWindows[4] = {14, 12, 10, 8};
 
 static const int RazorDepth = 1;
 static const int RazorMargin = 350;
