ee69c46ddafd690896be2a78a24d39f58d7f8647
AndyGrant <Andrew@grantnet.us>
2019-01-08 17:41:09 -0500

    .

--- src/search.c
+++ src/search.c
@@ -531,8 +531,10 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         // Step 15B. Check Extensions. We extend captures and good quiets that
         // come from in check positions, so long as no other extensions occur
         extension += !RootNode
+                  && !extension
                   &&  inCheck
-                  && !extension;
+                  && (played == 1 || !isQuiet);
+
 
         // Step 15C. History Extensions. We extend quiet moves with strong
         // history scores for both counter move and followups. We only apply
