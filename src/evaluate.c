8d227c389c7df890de40893657f0a3803eb1cf92
AndyGrant <Andrew@grantnet.us>
2019-01-09 00:17:01 -0500

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -200,7 +200,7 @@ const int KingStorm[2][FILE_NB/2][RANK_NB] = {
     S(  -9,  -5), S(   7, -10), S(  15,   1), S(   1,   1)}},
 };
 
-const int FawnPawn = S( -10, -20);
+const int FawnPawn = S( -40, -75);
 
 /* King Safety Evaluation Terms */
 
