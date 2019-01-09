73421727cece61b8a576f91446cdc7e1efd2f875
AndyGrant <Andrew@grantnet.us>
2019-01-09 16:16:52 -0500

    .

--- src/search.c
+++ src/search.c
@@ -528,11 +528,10 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
                   && (ttBound & BOUND_LOWER)
                   &&  moveIsSingular(thread, ttMove, ttValue, undo, depth, height);
 
-        // Step 15B. Check Extensions. We extend captures and good quiets that
-        // come from in check positions, so long as no other extensions occur
+        // Step 15B. Check Extensions.
         extension += !RootNode
-                  &&  inCheck
-                  && !extension;
+                  && !extension
+                  && (inCheck && !board->kingAttackers);
 
         // Step 15C. History Extensions. We extend quiet moves with strong
         // history scores for both counter move and followups. We only apply
