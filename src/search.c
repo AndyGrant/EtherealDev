66e3c73762c3b2faaf6830c14408b11b69edccf8
AndyGrant <Andrew@grantnet.us>
2019-01-12 08:12:24 -0500

    .

--- src/search.c
+++ src/search.c
@@ -479,7 +479,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             R += !improving;
 
             // Reduce for Killers and Counters
-            if (!ttHit)
+            if (!ttHit || moveIsTactical(board, ttMove))
                 R -= move == movePicker.killer1
                   || move == movePicker.killer2
                   || move == movePicker.counter;
