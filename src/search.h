ec248397f034337d3d8667eba6a798466924587e
AndyGrant <Andrew@grantnet.us>
2019-01-12 22:28:42 -0500

    .

--- src/search.h
+++ src/search.h
@@ -100,8 +100,8 @@ static const int FollowUpMoveHistoryLimit[] = { -2000, -4000 };
 
 static const int LateMovePruningDepth = 8;
 static const int LateMovePruningCounts[2][9] = {
-    {  0,  3,  4,  7, 12, 16, 21, 28, 34},
-    {  0,  5,  7, 12, 18, 27, 38, 50, 65},
+    {  1,  2,  4,  7, 12, 16, 21, 28, 34},
+    {  2,  4,  7, 12, 18, 27, 38, 50, 65},
 };
 
 static const int SEEPruningDepth = 8;
