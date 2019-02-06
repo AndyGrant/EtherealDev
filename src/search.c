2529466d495cfc10e231d5ab1a03e545bcdf1c69
AndyGrant <Andrew@grantnet.us>
2019-02-06 17:47:29 -0500

    .

--- src/search.c
+++ src/search.c
@@ -417,8 +417,10 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // tried many quiets in this position already, and we don't expect
             // anything from this move, we can skip all the remaining quiets
             if (   depth <= LateMovePruningDepth
-                && quiets >= LateMovePruningCounts[improving][depth])
-                skipQuiets = 1;
+                && quiets >= LateMovePruningCounts[improving][depth]) {
+                if (skipQuiets) continue
+                else skipQuiets = 1;
+            }
 
             // Step 12C. Counter Move Pruning. Moves with poor counter
             // move history are pruned at near leaf nodes of the search.
