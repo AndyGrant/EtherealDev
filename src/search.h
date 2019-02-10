30498117b4e174a83aa7bc151b98198afa2a9110
AndyGrant <Andrew@grantnet.us>
2019-02-09 20:08:21 -0500

    .

--- src/search.h
+++ src/search.h
@@ -93,10 +93,10 @@ static const int FutilityPruningDepth = 8;
 static const int FutilityPruningHistoryLimit[] = { 12000, 6000 };
 
 static const int CounterMovePruningDepth[] = { 3, 2 };
-static const int CounterMoveHistoryLimit[] = { 0, -1000 };
+static const int CounterMoveHistoryLimit[] = { 0, -1250 };
 
 static const int FollowUpMovePruningDepth[] = { 3, 2 };
-static const int FollowUpMoveHistoryLimit[] = { -2000, -4000 };
+static const int FollowUpMoveHistoryLimit[] = { -2250, -4500 };
 
 static const int LateMovePruningDepth = 8;
 static const int LateMovePruningCounts[2][9] = {
