d604966cd4cfb9a56e7574e7c1d3ebddd36b2f7a
AndyGrant <Andrew@grantnet.us>
2019-02-16 08:46:37 -0500

    .

--- src/movepicker.c
+++ src/movepicker.c
@@ -333,88 +333,72 @@ void evaluateQuietMoves(MovePicker* mp){
 
 int moveIsPsuedoLegal(Board* board, uint16_t move){
 
-    int colour = board->turn;
-    int to     = MoveTo(move);
-    int from   = MoveFrom(move);
-    int type   = MoveType(move);
-    int ptype  = MovePromoType(move);
-    int ftype  = pieceType(board->squares[from]);
+    const int colour = board->turn;
+    const int to     = MoveTo(move);
+    const int from   = MoveFrom(move);
+    const int type   = MoveType(move);
+    const int ptype  = MovePromoType(move);
+    const int ftype  = pieceType(board->squares[from]);
 
-    uint64_t friendly = board->colours[ colour];
-    uint64_t enemy    = board->colours[!colour];
-    uint64_t occupied = friendly | enemy;
-    uint64_t left, right, forward;
+    const uint64_t friendly = board->colours[ colour];
+    const uint64_t enemy    = board->colours[!colour];
+    const uint64_t occupied = friendly | enemy;
 
-    // Quick check against obvious illegal moves
-    if (move == NULL_MOVE || move == NONE_MOVE)
+    // Check for clearly illegal moves; Those which are designated as special
+    // moves, those which capture one of our own pieces, those which move a
+    // piece that does not belong to the moving player, and any moves which
+    // have the promotion type flags set without being actual promotion moves
+    if (    move == NONE_MOVE || move == NULL_MOVE
+        ||  pieceColour(board->squares[to]) == colour
+        ||  pieceColour(board->squares[from]) != colour
+        || (ptype != PROMOTE_TO_KNIGHT && type != PROMOTION_MOVE))
         return 0;
-
-    // Verify the moving piece is our own
-    if (pieceColour(board->squares[from]) != colour)
-        return 0;
-
-    // Non promotions should be marked as PROMOTE_TO_KNIGHT
-    if (ptype != PROMOTE_TO_KNIGHT && type != PROMOTION_MOVE)
-        return 0;
-
 
     // Knight, Rook, Bishop, and Queen moves are legal so long as the
     // move type is NORMAL and the destination is an attacked square
 
     if (ftype == KNIGHT)
-        return type == NORMAL_MOVE
-            && testBit(knightAttacks(from) & ~friendly, to);
+        return type == NORMAL_MOVE && testBit(knightAttacks(from), to);
 
     if (ftype == BISHOP)
-        return type == NORMAL_MOVE
-            && testBit(bishopAttacks(from, occupied) & ~friendly, to);
+        return type == NORMAL_MOVE && testBit(bishopAttacks(from, occupied), to);
 
     if (ftype == ROOK)
-        return type == NORMAL_MOVE
-            && testBit(rookAttacks(from, occupied) & ~friendly, to);
+        return type == NORMAL_MOVE && testBit(rookAttacks(from, occupied), to);
 
     if (ftype == QUEEN)
-        return type == NORMAL_MOVE
-            && testBit(queenAttacks(from, occupied) & ~friendly, to);
+        return type == NORMAL_MOVE && testBit(queenAttacks(from, occupied), to);
 
     if (ftype == PAWN){
 
-        // Throw out castle moves with our pawn
+        uint64_t attacks = pawnAttacks(colour, from);
+        uint64_t forward = pawnAdvance(1ull << from, occupied, colour);
+        uint64_t relativeRank3 = colour == WHITE ? RANK_3 : RANK_6;
+
+        // Castle moves are always illegal for pawns
         if (type == CASTLE_MOVE)
             return 0;
-
-        // Look at the squares which our pawn threatens
-        left  = pawnLeftAttacks(1ull << from, ~0ull, colour);
-        right = pawnRightAttacks(1ull << from, ~0ull, colour);
 
         // Enpass moves are legal if our to square is the enpass
         // square and we could attack a piece on the enpass square
         if (type == ENPASS_MOVE)
-            return   to == board->epSquare
-                && ((left | right) & (1ull << board->epSquare));
-
-        // Ensure that left and right are now captures, compute advances
-        left = left & enemy;
-        right = right & enemy;
-        forward = pawnAdvance(1ull << from, occupied, colour);
+            return to == board->epSquare && testBit(attacks, to);
 
         // Promotion moves are legal if we can move to one of the promotion
         // ranks, defined by PROMOTION_RANKS, independent of moving colour
         if (type == PROMOTION_MOVE)
-            return !!(PROMOTION_RANKS & (left | right | forward) & (1ull << to));
+            return testBit(PROMOTION_RANKS & (forward | (attacks & enemy)), to);
 
-        // Add the double advance to forward
-        forward |= pawnAdvance(forward & (!colour ? RANK_3 : RANK_6), occupied, colour);
-
-        // Normal moves are legal if we can move there
-        return !!((left | right | forward) & (1ull << to) & ~PROMOTION_RANKS);
+        // Normal moves are legal if we can move there without promoting
+        forward |= pawnAdvance(forward & relativeRank3, occupied, colour);
+        return testBit(~PROMOTION_RANKS & (forward | (attacks & enemy)), to);
     }
 
     if (ftype == KING){
 
         // Normal moves are legal if to square is a valid target
         if (type == NORMAL_MOVE)
-            return testBit(kingAttacks(from) & ~friendly, to);
+            return testBit(kingAttacks(from), to)dw;
 
         // Kings cannot castle or promote
         if (type == ENPASS_MOVE || type == PROMOTION_MOVE)
