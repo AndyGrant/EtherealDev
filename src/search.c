0ca8cb4dd21efb297102121ff37c5f5d51189543
AndyGrant <Andrew@grantnet.us>
2019-02-19 07:59:23 -0500

    .

--- src/search.c
+++ src/search.c
@@ -47,7 +47,7 @@
 #include "windows.h"
 
 
-int LMRTable[64][64]; // Late Move Reductions, LMRTable[depth][played]
+int8_t LMRTable[64][64]; // Late Move Reductions, LMRTable[depth][played]
 
 volatile int ABORT_SIGNAL; // Global ABORT flag for threads
 
