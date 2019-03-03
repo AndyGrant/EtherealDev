a6fa4d3404967a1fbe329e1a07eae2b6ac99f055
AndyGrant <Andrew@grantnet.us>
2019-03-03 15:30:50 -0500

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -203,15 +203,16 @@ const int KingStorm[2][FILE_NB/2][RANK_NB] = {
 /* King Safety Evaluation Terms */
 
 const int KSAttackWeight[]  = { 0, 16, 6, 10, 8, 0 };
-const int KSAttackValue     =   44;
-const int KSWeakSquares     =   38;
+const int KSAttackValue     =   42;
+const int KSWeakSquares     =   36;
+const int KSTargetSquares   =   16;
 const int KSFriendlyPawns   =  -22;
 const int KSNoEnemyQueens   = -276;
 const int KSSafeQueenCheck  =   95;
 const int KSSafeRookCheck   =   94;
 const int KSSafeBishopCheck =   51;
 const int KSSafeKnightCheck =  123;
-const int KSAdjustment      =  -18;
+const int KSAdjustment      =  -24;
 
 /* Passed Pawn Evaluation Terms */
 
@@ -697,6 +698,7 @@ int evaluateKings(EvalInfo *ei, Board *board, int colour) {
         count += KSAttackValue     * scaledAttackCounts
                + KSWeakSquares     * popcount(weak & ei->kingAreas[US])
                + KSFriendlyPawns   * popcount(myPawns & ei->kingAreas[US] & ~weak)
+               + KSTargetSquares   * popcount(board->colours[US] & ei->attackedBy2[THEM] & ei->kingAreas[US])
                + KSNoEnemyQueens   * !enemyQueens
                + KSSafeQueenCheck  * popcount(queenChecks)
                + KSSafeRookCheck   * popcount(rookChecks)
