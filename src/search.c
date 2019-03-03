0a78337cd0137572db4b02ebc0cfc89bfaf011db
AndyGrant <Andrew@grantnet.us>
2019-03-03 16:01:36 -0500

    .

--- src/search.c
+++ src/search.c
@@ -455,6 +455,9 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
             // Increase for non improving nodes
             R += !improving;
 
+            // Decrease reduction when making different moves with the tt piece
+            R -= ttHit && MoveFrom(ttMove) == MoveFrom(move);
+
             // Reduce for Killers and Counters
             R -= move == movePicker.killer1
               || move == movePicker.killer2
