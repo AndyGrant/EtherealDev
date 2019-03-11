08eed0ecd54b476e3c296b4ba23eec9b3113cffb
AndyGrant <Andrew@grantnet.us>
2019-03-11 02:44:43 -0400

    .

--- src/time.h
+++ src/time.h
@@ -31,6 +31,6 @@ int terminateSearchEarly(Thread *thread);
 static const double PVFactorCount  = 8;
 static const double PVFactorWeight = 0.085;
 
-static const double ScoreFactorWeight = 0.025;
+static const double ScoreFactorWeight = 0.040;
 
 #endif
