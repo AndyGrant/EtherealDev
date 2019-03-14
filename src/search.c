42ab78ed3d6f99e4e8256cd5df24a3276b92c767
AndyGrant <Andrew@grantnet.us>
2019-03-14 17:26:14 -0400

    .

--- src/search.c
+++ src/search.c
@@ -200,7 +200,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     int ttHit, ttValue = 0, ttEval = 0, ttDepth = 0, ttBound = 0;
     int R, newDepth, rAlpha, rBeta, oldAlpha = alpha;
     int inCheck, isQuiet, improving, extension, skipQuiets = 0;
-    int eval, value = -MATE, best = -MATE, futilityMargin, seeMargin[2];
+    int eval, value = -MATE, best = -MATE, seeMargin[2];
     uint16_t move, ttMove = NONE_MOVE, bestMove = NONE_MOVE, quietsTried[MAX_MOVES];
     MovePicker movePicker;
 
@@ -300,9 +300,6 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
     eval = thread->evalStack[height] = ttHit && ttEval != VALUE_NONE ? ttEval
                                      : evaluateBoard(board, &thread->pktable);
 
-    // Futility Pruning Margin
-    futilityMargin = eval + FutilityMargin * depth;
-
     // Static Exchange Evaluation Pruning Margins
     seeMargin[0] = SEENoisyMargin * depth * depth;
     seeMargin[1] = SEEQuietMargin * depth;
@@ -401,8 +398,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
 
             // Step 12A. Futility Pruning. If our score is far below alpha, and we
             // don't expect anything from this move, we can skip all other quiets
-            if (   futilityMargin <= alpha
-                && depth <= FutilityPruningDepth
+            if (   depth <= FutilityPruningDepth
+                && eval + MAX(0, FutilityMargin * (depth + MAX(-2, MIN(2, (hist + cmhist + fmhist) / 10000)))) <= alpha
                 && hist + cmhist + fmhist < FutilityPruningHistoryLimit[improving])
                 skipQuiets = 1;
 
