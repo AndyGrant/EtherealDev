ca2da4b60beab8bb3f445c93f38254311268845e
AndyGrant <Andrew@grantnet.us>
2019-03-25 08:18:24 -0400

    .

--- src/search.c
+++ src/search.c
@@ -214,7 +214,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         return qsearch(thread, pv, alpha, beta, height);
 
     // Ensure positive depth
-    depth = MAX(0, depth);
+    depth = MAX(1, depth);
 
     // Updates for UCI reporting
     thread->seldepth = RootNode ? 0 : MAX(thread->seldepth, height);
@@ -478,7 +478,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         // an early move has excellent continuation history, or when we have a move
         // from the transposition table which appears to beat all other moves by a
         // relativly large margin,
-        extension =  (inCheck)
+        extension =  (inCheck && !skipQuiets)
                   || (isQuiet && quiets <= 4 && cmhist >= 10000 && fmhist >= 10000)
                   || (singular && moveIsSingular(thread, ttMove, ttValue, depth, height));
 
