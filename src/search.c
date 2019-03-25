219e5d6fb56203ddc3632d647d834371fb51107b
AndyGrant <Andrew@grantnet.us>
2019-03-25 10:08:45 -0400

    .

--- src/search.c
+++ src/search.c
@@ -444,7 +444,7 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
 
         // Step 14. Late Move Reductions. Compute the reduction,
         // allow the later steps to perform the reduced searches
-        if (isQuiet && depth > 2 && played > 1){
+        if (isQuiet && depth > 2 && best > MATED_IN_MAX){
 
             R  = LMRTable[MIN(depth, 63)][MIN(played, 63)];
 
