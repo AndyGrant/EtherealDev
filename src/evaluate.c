4caa2c02c726b5ad41dec04b432d73c4e5ba05f6
AndyGrant <Andrew@grantnet.us>
2019-03-30 09:53:16 -0400

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -210,7 +210,7 @@ const int KSSafeQueenCheck  =   95;
 const int KSSafeRookCheck   =   94;
 const int KSSafeBishopCheck =   51;
 const int KSSafeKnightCheck =  123;
-const int KSDefenders       =  -15;
+const int KSDefenders       =  -25;
 const int KSAdjustment      =  -18;
 
 /* Passed Pawn Evaluation Terms */
