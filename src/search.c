a47ac4ed00a7533ec5faa3e34efc9b5c33a2ed33
AndyGrant <Andrew@grantnet.us>
2019-06-19 20:10:07 -0400

    .

--- src/search.c
+++ src/search.c
@@ -473,8 +473,8 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         // an early move has excellent continuation history, or when we have a move
         // from the transposition table which appears to beat all other moves by a
         // relativly large margin,
-        extension =  (inCheck && (!isQuiet || depth <= 2 || played == 1))
-                  || (isQuiet && quiets <= 4 && cmhist >= 10000 && fmhist >= 10000)
+        extension =  (isQuiet && quiets <= 4 && cmhist >= 10000 && fmhist >= 10000)
+                  || (inCheck && (PvNode || (!isQuiet || depth <= 2 || played == 1)))
                   || (singular && moveIsSingular(thread, ttMove, ttValue, depth, height));
 
         // Factor the extension into the new depth. Do not extend at the root
