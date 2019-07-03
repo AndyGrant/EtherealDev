eea53fb3e04a124c43f0e820d23450b0bec01f7e
AndyGrant <Andrew@grantnet.us>
2019-07-03 15:31:10 -0400

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -253,15 +253,15 @@ const int KingStorm[2][FILE_NB/2][RANK_NB] = {
 
 /* King Safety Evaluation Terms */
 
-const int KSAttackWeight[]  = { 0, 16, 6, 10, 8, 0 };
-const int KSAttackValue     =   44;
-const int KSWeakSquares     =   38;
+const int KSAttackWeight[]  = { 0, 17, 6, 10, 5, 0 };
+const int KSAttackValue     =   42;
+const int KSWeakSquares     =   15;
 const int KSFriendlyPawns   =  -22;
 const int KSNoEnemyQueens   = -276;
-const int KSSafeQueenCheck  =   95;
-const int KSSafeRookCheck   =   94;
+const int KSSafeQueenCheck  =   58;
+const int KSSafeRookCheck   =   93;
 const int KSSafeBishopCheck =   51;
-const int KSSafeKnightCheck =  123;
+const int KSSafeKnightCheck =  122;
 const int KSAdjustment      =  -18;
 
 /* Passed Pawn Evaluation Terms */
