f95cdeeb12b4300bf7afd69932cfcdc88aa1c1ea
AndyGrant <Andrew@grantnet.us>
2019-10-11 18:58:26 -0400

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -303,10 +303,10 @@ const int ThreatByPawnPush           = S(  15,  21);
 /* Complexity Evaluation Terms */
 
 const int ComplexityPassedPawns = S(   0,   2);
-const int ComplexityTotalPawns  = S(   0,   7);
-const int ComplexityPawnFlanks  = S(   0,  49);
-const int ComplexityPawnEndgame = S(   0,  34);
-const int ComplexityAdjustment  = S(   0,-110);
+const int ComplexityTotalPawns  = S(   0,   9);
+const int ComplexityPawnFlanks  = S(   0,  87);
+const int ComplexityPawnEndgame = S(   0,  51);
+const int ComplexityAdjustment  = S(   0,-170);
 
 /* General Evaluation Terms */
 
