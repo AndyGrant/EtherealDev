65bfb9488284a30e9849059eba5f790ec938edfc
AndyGrant <Andrew@grantnet.us>
2019-10-30 23:25:38 -0400

    .

--- src/search.c
+++ src/search.c
@@ -338,7 +338,7 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
         &&  boardHasNonPawnMaterial(board, board->turn)
         && (!ttHit || !(ttBound & BOUND_UPPER) || ttValue >= beta)) {
 
-        R = 4 + depth / 6 + MIN(3, (eval - beta) / 200);
+        R = ((13 + depth) >> 2) + MIN(3, (eval - beta) / 185);
 
         apply(thread, board, NULL_MOVE, height);
         value = -search(thread, &lpv, -beta, -beta+1, depth-R, height+1);
