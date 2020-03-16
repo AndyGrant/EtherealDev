5f4cf13275f79e9dfecc61e03035fd4faa65de5c
AndyGrant <Andrew@grantnet.us>
2020-03-16 11:29:01 -0400

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -461,7 +461,9 @@ int evaluatePawns(EvalInfo *ei, Board *board, int colour) {
 
         // Apply a bonus for pawns which will become passers by advancing a
         // square then exchanging our supporters with the remaining stoppers
-        else if (!leftovers && popcount(pushSupport) >= popcount(pushThreats)) {
+        else if (!leftovers
+            && !several(forwardFileMasks(US, sq) & myPawns)
+            &&  popcount(pushSupport) >= popcount(pushThreats)) {
             flag = popcount(support) >= popcount(threats);
             pkeval += PawnCandidatePasser[flag][relativeRankOf(US, sq)];
             if (TRACE) T.PawnCandidatePasser[flag][relativeRankOf(US, sq)][US]++;
