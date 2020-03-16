5007588444d258634399d5d14f36cce85ee025c8
AndyGrant <Andrew@grantnet.us>
2020-03-16 09:06:58 -0400

    .

--- src/search.c
+++ src/search.c
@@ -426,14 +426,12 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
 
             // Step 11D (~8 elo). Counter Move Pruning. Moves with poor counter
             // move history are pruned at near leaf nodes of the search.
-            if (   depth <= CounterMovePruningDepth[improving]
-                && cmhist < CounterMoveHistoryLimit[improving])
+            if (depth <= CounterMovePruningDepth && cmhist < CounterMoveHistoryLimit)
                 continue;
 
             // Step 11E (~1.5 elo). Follow Up Move Pruning. Moves with poor
             // follow up move history are pruned at near leaf nodes of the search.
-            if (   depth <= FollowUpMovePruningDepth[improving]
-                && fmhist < FollowUpMoveHistoryLimit[improving])
+            if (depth <= FollowUpMovePruningDepth && fmhist < FollowUpMoveHistoryLimit)
                 continue;
         }
 
