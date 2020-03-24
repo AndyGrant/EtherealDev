9ffab3a160b345d4f66679f9e048f958d4b47e98
AndyGrant <Andrew@grantnet.us>
2020-03-24 08:24:17 -0400

    .

--- src/search.c
+++ src/search.c
@@ -352,8 +352,8 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
         && (!ttHit || !(ttBound & BOUND_UPPER) || ttValue >= beta)) {
 
         R = 4 + depth / 6 + MIN(3, (eval - beta) / 200)
-          + 2 * (thread->moveStack[height-2] == NULL_MOVE)
-          + 2 * (thread->moveStack[height-4] == NULL_MOVE);
+          + 4 * (thread->moveStack[height-2] == NULL_MOVE)
+          + 4 * (thread->moveStack[height-4] == NULL_MOVE);
 
         apply(thread, board, NULL_MOVE, height);
         value = -search(thread, &lpv, -beta, -beta+1, depth-R, height+1);
