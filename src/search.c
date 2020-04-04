9d78f7af1d9563d2cfbf39907a93de3df0a9b823
AndyGrant <Andrew@grantnet.us>
2020-04-04 17:30:55 -0400

    PvNode && improving

--- src/search.c
+++ src/search.c
@@ -474,7 +474,7 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
 
         extension = singular
                   ? moveIsSingular(thread, ttMove, ttValue, depth, height, beta, &multiCut)
-                  : inCheck || (isQuiet && quietsSeen <= 4 && cmhist >= 10000 && fmhist >= 10000);
+                  : inCheck || (isQuiet && PvNode && improving && cmhist + fmhist >= 24000);
 
         // Step 14. MultiCut. Sometimes candidate Singular moves are shown to be non-Singular.
         // If this happens, and the rBeta used for that proof is greater than beta, then we
