0ec95480f2e2d75aa6943f8f8e12b9d3e42fc9f3
AndyGrant <Andrew@grantnet.us>
2020-04-09 08:15:30 -0400

    .

--- src/search.c
+++ src/search.c
@@ -498,6 +498,8 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
             // Increase for King moves that evade checks
             R += inCheck && pieceType(board->squares[MoveTo(move)]) == KING;
 
+            R += inCheck && board->kingAttackers;
+
             // Reduce for Killers and Counters
             R -= movePicker.stage < STAGE_QUIET;
 
