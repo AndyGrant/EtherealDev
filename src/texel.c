c7f329a7b3dd3d298ab872084c34f81268c7287a
Andrew Grant <andrew@grantnet.us>
2020-04-22 00:38:09 -0400

    Bench 5914042

--- src/texel.c
+++ src/texel.c
@@ -413,7 +413,7 @@ double linearEvaluation(TexelEntry *te, TexelVector params) {
 }
 
 double sigmoid(double K, double S) {
-    return 1.0 / (1.0 + pow(10.0, -K * S / 400.0));
+    return 1.0 / (1.0 + exp(-K * S / 400.0));
 }
 
 void printParameters(TexelVector params, TexelVector cparams) {
