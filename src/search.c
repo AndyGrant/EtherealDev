18de97bad9727f42934633bfdbc3c0f57bd90b74
Andrew Grant <andrew@grantnet.us>
2020-04-28 18:33:58 -0400

    Bench 6168233

--- src/search.c
+++ src/search.c
@@ -812,7 +812,7 @@ int singularity(Thread *thread, MovePicker *mp, int ttValue, int depth, int beta
     if (value > rBeta && rBeta >= beta) {
         if (!moveIsTactical(board, move))
             updateKillerMoves(thread, mp->height, move);
-        if (!moveIsTactical(board, mp->tableMove))
+        else if (!moveIsTactical(board, mp->tableMove))
             updateKillerMoves(thread, mp->height, mp->tableMove);
         mp->stage = STAGE_DONE;
     }
