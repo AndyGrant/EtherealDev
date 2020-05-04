3850f17bf33cf271eb0015b2901ac8b1ac1b7f8f
Andrew Grant <andrew@grantnet.us>
2020-05-03 22:03:44 -0400

    Bench 5662440

--- src/texel.h
+++ src/texel.h
@@ -30,7 +30,7 @@
 #define LEARNING     (    5.0) // Learning rate
 #define LRDROPRATE   (   1.25) // Cut LR by this each failure
 #define BATCHSIZE    (  16384) // FENs per mini-batch
-#define NPOSITIONS   (5888224) // Total FENS in the book
+#define NPOSITIONS   (5820614) // Total FENS in the book
 
 #define STACKSIZE ((int)((double) NPOSITIONS * NTERMS / 32))
 
