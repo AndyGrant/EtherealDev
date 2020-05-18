35ab1ddcbd6420505026c8c6d8d8801f4e29ce50
Andrew Grant <andrew@grantnet.us>
2020-05-18 18:44:13 -0400

    Bench 5915179

--- src/move.c
+++ src/move.c
@@ -509,7 +509,7 @@ int moveBestCaseValue(Board *board) {
         targets = board->pieces[piece] & targetable;
 
         while (targets)
-            if (squareIsAttacked(board, THEM, poplsb(&targets)))
+            if (piece < ROOK || squareIsAttacked(board, THEM, poplsb(&targets)))
                 return value + SEEPieceValues[piece];
     }
 
