8fd53defd79d811affef365b1a49f4b065db35d1
Andrew Grant <andrew@grantnet.us>
2020-05-27 00:48:31 -0400

    Bench 5535088

--- src/evaluate.c
+++ src/evaluate.c
@@ -959,7 +959,8 @@ int evaluateThreats(EvalInfo *ei, Board *board, int colour) {
     if (TRACE) T.ThreatMinorAttackedByPawn[US] += count;
 
     // Penalty for any minor threat against minor pieces
-    count = popcount((knights | bishops) & attacksByMinors);
+    count = popcount(knights & ei->attackedBy[THEM][BISHOP])
+          + popcount(bishops & ei->attackedBy[THEM][KNIGHT]);
     eval += count * ThreatMinorAttackedByMinor;
     if (TRACE) T.ThreatMinorAttackedByMinor[US] += count;
 
