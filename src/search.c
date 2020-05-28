88d48a90a19a1a37ce3c0f32b99c346156edf179
Andrew Grant <andrew@grantnet.us>
2020-05-28 05:44:19 -0400

    Bench 5687421

--- src/search.c
+++ src/search.c
@@ -572,7 +572,7 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
 
     // Step 19 (~760 elo). Update History counters on a fail high for a quiet move
     if (best >= beta && !moveIsTactical(board, bestMove)) {
-        int bonus = eval + 16 * depth < beta ? 2 * depth * depth : depth * depth;
+        int bonus = moveIsTactical(board, ttMove) ? 2 * depth * depth : depth * depth;
         updateHistoryHeuristics(thread, quietsTried, quietsPlayed, height, bonus);
     }
 
