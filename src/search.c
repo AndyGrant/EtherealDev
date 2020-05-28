f2ec99a414584911404d43c6d93ca2adea3de409
Andrew Grant <andrew@grantnet.us>
2020-05-28 06:12:31 -0400

    Bench 6227332

--- src/search.c
+++ src/search.c
@@ -480,7 +480,7 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
         // with very strong continuation histories, so long as they are along the PV line
 
         extension =  singular ? singularity(thread, &movePicker, ttValue, depth, beta)
-                  : (inCheck && (movePicker.stage != STAGE_BAD_NOISY || eval > beta))
+                  : (inCheck && (movePicker.stage != STAGE_BAD_NOISY || eval < beta))
                  || (isQuiet && PvNode && cmhist > HistexLimit && fmhist > HistexLimit);
 
 
