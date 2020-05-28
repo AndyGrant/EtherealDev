64f5340f1889a13d31844f0c4490c1f5110036b2
Andrew Grant <andrew@grantnet.us>
2020-05-28 05:29:46 -0400

    Bench 5472565

--- src/search.c
+++ src/search.c
@@ -817,8 +817,14 @@ int singularity(Thread *thread, MovePicker *mp, int ttValue, int depth, int beta
 
     // MultiCut. We signal the Move Picker to terminate the search
     if (value > rBeta && rBeta >= beta) {
+
+        if (!moveIsTactical(board, mp->tableMove))
+            updateKillerMoves(thread, mp->height, mp->tableMove);
+
         if (!moveIsTactical(board, move))
             updateKillerMoves(thread, mp->height, move);
+
+
         mp->stage = STAGE_DONE;
     }
 
