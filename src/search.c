8d16ac789a3a952c2ff9ecb5af2ce54321c9233a
Andrew Grant <andrew@grantnet.us>
2020-06-07 08:00:14 -0400

    Bench 5372946

--- src/search.c
+++ src/search.c
@@ -511,9 +511,9 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
             R  = LMRTable[MIN(depth, 63)][MIN(played, 63)];
 
             // Increase for non PV, non improving, and extended nodes
-            R += !PvNode + !improving + extension;
+            R += !PvNode + !improving + extension + trimming;
 
-            R += trimming && played > depth;
+            R += trimming && (hist + cmhist + fmhist < 0);
 
             // Increase for King moves that evade checks
             R += inCheck && pieceType(board->squares[MoveTo(move)]) == KING;
