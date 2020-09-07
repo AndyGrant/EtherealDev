af7d700fc942667770034a9e36421c76bc92c542
Andrew Grant <andrew@grantnet.us>
2020-09-07 01:04:36 -0400

    Bench 4568883

--- src/search.c
+++ src/search.c
@@ -507,6 +507,9 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
             // Increase for King moves that evade checks
             R += inCheck && pieceType(board->squares[MoveTo(move)]) == KING;
 
+            // Increase for Queen moves that block a check in weak positions
+            R += inCheck && !improving && pieceType(board->squares[MoveTo(move)]) == QUEEN;
+
             // Reduce for Killers and Counters
             R -= movePicker.stage < STAGE_QUIET;
 
