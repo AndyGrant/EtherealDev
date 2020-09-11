5dda0ad7aeaafd62c426090b552018d66652cbb7
Andrew Grant <andrew@grantnet.us>
2020-09-11 10:11:10 -0400

    Bench 4818961

--- src/search.c
+++ src/search.c
@@ -424,7 +424,7 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, int h
             // have tried many quiets in this position already, and we don't expect
             // anything from this move, we can skip all the remaining quiets
             if (   depth <= LateMovePruningDepth
-                && quietsPlayed >= LateMovePruningCounts[improving][depth])
+                && quietsPlayed + 1 >= LateMovePruningCounts[improving][depth])
                 skipQuiets = 1;
 
             // Step 11D (~8 elo). Counter Move Pruning. Moves with poor counter
