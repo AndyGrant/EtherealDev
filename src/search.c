3ed6129688f60783759374b249a226d738986d8e
Andrew Grant <andrew@grantnet.us>
2020-09-11 08:32:49 -0400

    Bench 4110616

--- src/search.c
+++ src/search.c
@@ -54,7 +54,7 @@ void initSearch() {
     // Init Late Move Reductions Table
     for (int depth = 1; depth < 64; depth++)
         for (int played = 1; played < 64; played++)
-            LMRTable[depth][played] = 0.75 + log(depth) * log(played) / 2.25;
+            LMRTable[depth][played] = 0.85 + log(depth) * log(played) / 2.15;
 }
 
 void getBestMove(Thread *threads, Board *board, Limits *limits, uint16_t *best, uint16_t *ponder) {
--- src/search.h
+++ src/search.h
@@ -70,8 +70,8 @@ static const int FollowUpMoveHistoryLimit[] = { -2000, -4000 };
 
 static const int LateMovePruningDepth = 8;
 static const int LateMovePruningCounts[2][9] = {
-    {  0,  3,  4,  6, 10, 14, 19, 25, 31},
-    {  0,  5,  7, 11, 17, 26, 36, 48, 63},
+    {  0,  2,  3,  5,  9, 13, 18, 24, 30},
+    {  0,  4,  6, 10, 16, 25, 35, 47, 62},
 };
 
 static const int SEEPruningDepth = 9;
