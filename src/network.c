5248d14cb8c1e9e654495a701f6ca3f0dce5c78b
Andrew Grant <andrew@grantnet.us>
2020-09-16 14:15:11 -0400

    Null network of [224, 32, 1] trained on NeuralBook using a search d12 biasing

--- src/network.c
+++ src/network.c
@@ -8,7 +8,7 @@
 #include "bitboards.h"
 
 static const char *WeightsTXT[] = {
-    #include "net_224x32x1_nstatic.net"
+    #include "net_224x32x1_nsearch.net"
     ""
 };
 
@@ -250,5 +250,6 @@ int Network::evaluate(Board *board) {
     }
 
     int X = FeedForward(inputs);
+    return 0;
     return X;
 }
