986e9764800e12cfbee1c00aa11ddc2b0e1eac39
Andrew Grant <andrew@grantnet.us>
2020-09-20 12:40:53 -0400

    Additional epochs

--- src/network.h
+++ src/network.h
@@ -25,15 +25,15 @@
 
 #define PKNETWORK_INPUTS  (224)
 #define PKNETWORK_LAYER1  ( 32)
-#define PKNETWORK_OUTPUTS (  1)
+#define PKNETWORK_OUTPUTS (  2)
 
 typedef struct PKNetwork {
 
     // PKNetworks are of the form [Input, Hidden Layer 1, Output Layer]
-    // Our current Network is [224x32, 32x1]. The Network is trained to
-    // output a Score in CentiPawns, and thus Output Neurons need do not
-    // need activation functions. The 32x1 operation applys a ReLU, where
-    // as the 224x32 layer does not, since inputs are binary 0s and 1s
+    // Our current Network is [224x32, 32x2]. The Network is trained to
+    // output a Score in CentiPawns for the MG and EG, and thus Output
+    // Neurons need do not need activation functions. The 32x1 operation
+    // applys a ReLU, where as the 224x32 layer does not due to the inputs
 
     ALIGN64 float inputWeights[PKNETWORK_LAYER1][PKNETWORK_INPUTS];
     ALIGN64 float inputBiases[PKNETWORK_LAYER1];
