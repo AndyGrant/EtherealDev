325d550f144b504775e142e12188d1c9f50ff331
Andrew Grant <andrew@grantnet.us>
2021-01-11 06:22:05 -0500

    .

--- src/evaluate.c
+++ src/evaluate.c
@@ -453,7 +453,7 @@ int evaluateBoard(Thread *thread, Board *board) {
 
     // On some-what balanced positions, use just NNUE
     if (   !board->kingAttackers
-        &&  abs(ScoreEG(board->psqtmat)) <= 400) {
+        &&  abs(ScoreEG(board->psqtmat)) <= 550) {
         eval = nnue_evaluate(thread, board);
         hashed = board->turn == WHITE ? eval : -eval;
         storeCachedEvaluation(thread, board, hashed);
