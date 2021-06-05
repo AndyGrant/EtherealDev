addb67f52ab6b6afc3b3fa4e3982f79efdff3c8b
Andrew Grant <andrew@grantnet.us>
2021-06-05 08:22:43 -0400

    Bench 4250238

--- src/transposition.c
+++ src/transposition.c
@@ -171,7 +171,7 @@ void storeTTEntry(uint64_t hash, uint16_t move, int value, int eval, int depth, 
     // an exact bound or depth that is nearly as good as the old one
     if (   bound != BOUND_EXACT
         && hash16 == replace->hash16
-        && depth < replace->depth - 3)
+        && 2 * depth <= replace->depth)
         return;
 
     // Finally, copy the new data into the replaced slot
