4b6bb8df049f13a6ec737d4eb97f5888be197fec
Andrew Grant <andrew@grantnet.us>
2021-06-12 23:26:21 -0400

    Bench 3893983

--- src/search.h
+++ src/search.h
@@ -75,7 +75,7 @@ static const int LateMovePruningDepth = 8;
 
 static const int SEEPruningDepth = 9;
 static const int SEEQuietMargin = -64;
-static const int SEENoisyMargin = -19;
+static const int SEENoisyMargin = -16;
 static const int SEEPieceValues[] = {
      100,  450,  450,  675,
     1300,    0,    0,    0,
