31aa1efb8d605decbc8ffd60bc21145cfc0ecf84
Andrew Grant <andrew@grantnet.us>
2021-06-26 03:54:58 -0400

    Bench 5648987

--- src/search.c
+++ src/search.c
@@ -320,9 +320,9 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
     seeMargin[1] = SEEQuietMargin * depth;
 
     // Improving if our static eval increased in the last move
-    improving = thread->height >= 2
-             && eval > thread->evalStack[thread->height-2]
-             && thread->evalStack[thread->height-2] != VALUE_NONE;
+    improving = !inCheck && thread->height >= 2
+             && (  eval > thread->evalStack[thread->height-2]
+                || thread->evalStack[thread->height-2] != VALUE_NONE);
 
     // Reset Killer moves for our children
     thread->killers[thread->height+1][0] = NONE_MOVE;
