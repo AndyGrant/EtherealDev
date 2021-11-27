2449a167d31a1276330bfccfb12047270ecb5ea9
Andrew Grant <andrew@grantnet.us>
2021-11-27 15:06:03 -0500

    Bench 4304840

--- src/search.c
+++ src/search.c
@@ -590,10 +590,9 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
             // Increase for King moves that evade checks
             R += inCheck && pieceType(board->squares[MoveTo(move)]) == KING;
 
-            // Increase for moves which place us in risk of a losing capture
+            // ??????
             R -=  MoveType(move) == NORMAL_MOVE
-              &&  testBit(threat_info[pieceType(board->squares[MoveTo(move)])], MoveFrom(move))
-              && !testBit(threat_info[pieceType(board->squares[MoveTo(move)])],   MoveTo(move));
+              &&  testBit(threat_info[pieceType(board->squares[MoveTo(move)])], MoveFrom(move));
 
             // Reduce for Killers and Counters
             R -= movePicker.stage < STAGE_QUIET;
