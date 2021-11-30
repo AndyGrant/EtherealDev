6de6f6149ff4b386aef728c03c6180a0fb9e2745
Andrew Grant <andrew@grantnet.us>
2021-11-30 15:00:56 -0500

    Bench 3988949

--- src/search.c
+++ src/search.c
@@ -242,6 +242,8 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
     const int RootNode = (thread->height == 0);
     Board *const board = &thread->board;
 
+    const uint64_t Checkers = board->kingAttackers;
+
     unsigned tbresult;
     int hist = 0, cmhist = 0, fmhist = 0;
     int movesSeen = 0, quietsPlayed = 0, capturesPlayed = 0, played = 0;
@@ -562,6 +564,11 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
 
             // Increase for King moves that evade checks
             R += inCheck && pieceType(board->squares[MoveTo(move)]) == KING;
+
+            // Increase when blocking a weaker checking piece with a Queen
+            R +=   inCheck
+              && !(Checkers & board->pieces[QUEEN])
+              &&   pieceType(board->squares[MoveTo(move)]) == QUEEN;
 
             // Reduce for Killers and Counters
             R -= movePicker.stage < STAGE_QUIET;
