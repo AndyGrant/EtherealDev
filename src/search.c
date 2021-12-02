b02c18844668cca8b9bbcf466fdf069759be4094
Andrew Grant <andrew@grantnet.us>
2021-12-01 19:50:38 -0500

    Bench 4337972

--- src/search.c
+++ src/search.c
@@ -396,7 +396,7 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
         &&  boardHasNonPawnMaterial(board, board->turn)
         && (!ttHit || !(ttBound & BOUND_UPPER) || ttValue >= beta)) {
 
-        R = 4 + depth / 6 + MIN(3, (eval - beta) / 200);
+        R = 3 + (depth + 3) / 6 + MIN(3, (eval - beta) / 200);
         R += thread->states[thread->height-1].tactical;
 
         apply(thread, board, NULL_MOVE);
