cf28810ee8b5d6c91c2c33806fd64bdd2fcca954
Andrew Grant <andrew@grantnet.us>
2021-12-17 04:09:23 -0500

    Bench 3739937

--- src/search.h
+++ src/search.h
@@ -62,7 +62,7 @@ static const int FutilityPruningDepth = 8;
 static const int FutilityMarginBase = 92;
 static const int FutilityMarginPerDepth = 59;
 static const int FutilityMarginNoHistory = 158;
-static const int FutilityPruningHistoryLimit[] = { 12000, 6000 };
+static const int FutilityPruningHistoryLimit[] = { 15000, 7500 };
 
 static const int CounterMovePruningDepth[] = { 3, 2 };
 static const int CounterMoveHistoryLimit[] = { 0, -1000 };
