4ad406d07fec5def2f4b03f2bc3162bc86db8164
Andrew Grant <andrew@grantnet.us>
2022-01-01 04:42:40 -0500

    Bench 4034459

--- src/search.c
+++ src/search.c
@@ -227,7 +227,7 @@ void aspirationWindow(Thread *thread) {
 
         // Search failed high, adjust window and reduce depth
         else if (pv->score >= beta) {
-            beta = MIN(MATE, beta + delta);
+            beta = MAX(pv->score, MIN(MATE, beta + delta));
             depth = depth - (abs(pv->score) <= MATE / 2);
         }
 
