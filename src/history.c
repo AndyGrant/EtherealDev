7492157d30dfdc6af9f96b9a119cbd74f2cfce6e
Andrew Grant <andrew@grantnet.us>
2022-05-07 02:51:24 -0400

    Bench 3211319

--- src/history.c
+++ src/history.c
@@ -139,8 +139,8 @@ void get_capture_histories(Thread *thread, uint16_t *moves, int *scores, int sta
 
     for (int i = start; i < start + length; i++)
         scores[i] = 64000 + get_capture_history(thread, moves[i])
-                  + 10000 * (MoveTo((ns-1)->move) == MoveTo(moves[i]))
-                  + MVVAugment[history_captured_piece(thread, moves[i])];
+                  + MVVAugment[history_captured_piece(thread, moves[i])]
+                  + 10000 * ((ns-1)->tactical && MoveTo((ns-1)->move) == MoveTo(moves[i]));
 }
 
 void update_capture_histories(Thread *thread, uint16_t best, uint16_t *moves, int length, int depth) {
