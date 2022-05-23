5756e9c9ba3617692c3511ae1dfa813756f08f54
Andrew Grant <andrew@grantnet.us>
2022-05-23 16:19:48 -0400

    Bench 3250199

--- src/search.c
+++ src/search.c
@@ -155,9 +155,10 @@ void initSearch() {
         for (int played = 1; played < 64; played++)
             LMRTable[depth][played] = 0.75 + log(depth) * log(played) / 2.25;
 
+    // Init Late Move Pruning Counts Table
     for (int depth = 1; depth < 9; depth++) {
-        LateMovePruningCounts[0][depth] = 2.5 + 2 * depth * depth / 4.5;
-        LateMovePruningCounts[1][depth] = 4.0 + 4 * depth * depth / 4.5;
+        LateMovePruningCounts[0][depth] = 1.5 + 2 * depth * depth / 4.5;
+        LateMovePruningCounts[1][depth] = 3.0 + 4 * depth * depth / 4.5;
     }
 }
 
