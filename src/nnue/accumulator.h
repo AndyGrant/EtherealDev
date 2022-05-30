8c8a4ae3c6982961a724ae8c5422ebbcfd9c0ea4
Andrew Grant <andrew@grantnet.us>
2022-05-30 17:22:44 -0400

    .

--- src/nnue/accumulator.h
+++ src/nnue/accumulator.h
@@ -23,9 +23,27 @@
 #include "types.h"
 #include "utils.h"
 
+#include "../bitboards.h"
 #include "../board.h"
 #include "../thread.h"
 #include "../types.h"
+
+int nnue_can_update(NNUEAccumulator *accum, Board *board);
+void nnue_refresh_accumulators(NNUEAccumulator *accum, Board *board, int wrelksq, int brelksq);
+void nnue_refresh_accumulator(NNUEAccumulator *accum, Board *board, int colour, int relksq);
+void nnue_update_accumulator(NNUEAccumulator *accum, Board *board, int wrelksq, int brelksq);
+
+static const int SquareToBucket[] = {
+     0,  1,  2,  3,  3,  2,  1,  0,
+     4,  5,  6,  7,  7,  6,  5,  4,
+     8,  9, 10, 11, 11, 10,  9,  8,
+     8,  9, 10, 11, 11, 10,  9,  8,
+    12, 12, 13, 13, 13, 13, 12, 12,
+    12, 12, 13, 13, 13, 13, 12, 12,
+    14, 14, 15, 15, 15, 15, 14, 14,
+    14, 14, 15, 15, 15, 15, 14, 14,
+};
+
 
 INLINE NNUEAccumulator* nnue_create_accumulators() {
     return align_malloc(sizeof(NNUEAccumulator) * (MAX_PLY + 4));
@@ -44,6 +62,12 @@ INLINE void nnue_push(Board *board) {
 }
 
 INLINE void nnue_move_piece(Board *board, int piece, int from, int to) {
+
+    if (   pieceType(piece) == KING
+        && SquareToBucket[from] == SquareToBucket[to]
+        && testBit(LEFT_FLANK, to) == testBit(LEFT_FLANK, from))
+        return;
+
     if (USE_NNUE && board->thread != NULL) {
         const int height = board->thread->height;
         NNUEAccumulator *accum = &board->thread->nnueStack[height+1];
@@ -59,8 +83,3 @@ INLINE void nnue_remove_piece(Board *board, int piece, int sq) {
     if (piece != EMPTY)
         nnue_move_piece(board, piece, sq, SQUARE_NB);
 }
-
-int nnue_can_update(NNUEAccumulator *accum, Board *board);
-void nnue_refresh_accumulators(NNUEAccumulator *accum, Board *board, int wrelksq, int brelksq);
-void nnue_refresh_accumulator(NNUEAccumulator *accum, Board *board, int colour, int relksq);
-void nnue_update_accumulator(NNUEAccumulator *accum, Board *board, int wrelksq, int brelksq);
