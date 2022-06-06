04eb9d11a13264626ca5eca0b1c5b1435f0defff
Andrew Grant <andrew@grantnet.us>
2022-06-06 19:06:21 -0400

    Bench 3134224

--- src/search.c
+++ src/search.c
@@ -502,6 +502,15 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
         }
     }
 
+    static const int RazoringDepth = 3;
+
+    // Step ?. PV Razoring when we have no TT move.
+    if (    PvNode
+        && !inCheck
+        &&  ttMove == NONE_MOVE
+        &&  depth <= RazoringDepth)
+        return qsearch(thread, pv, alpha, beta);
+
     // Step 11. Initialize the Move Picker and being searching through each
     // move one at a time, until we run out or a move generates a cutoff
     init_picker(&ns->mp, thread, ttMove);
