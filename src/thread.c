f142a80a50765a7384b90a605db1c88219b903b1
Andrew Grant <andrew@grantnet.us>
2022-06-26 04:56:07 -0400

    Bench 3081516

--- src/thread.c
+++ src/thread.c
@@ -100,7 +100,11 @@ void newSearchThreadPool(Thread *threads, Board *board, Limits *limits, TimeMana
         memcpy(&threads[i].board, board, sizeof(Board));
         threads[i].board.thread = &threads[i];
         threads[i].nnueStack[0].accurate = 0;
+
+        memset(threads[i].nodeStates, 0, sizeof(NodeState) * STACK_SIZE);
     }
+
+
 }
 
 uint64_t nodesSearchedThreadPool(Thread *threads) {
