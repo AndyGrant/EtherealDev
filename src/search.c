408fd71ecfdae73ec466a1b3e9c7d62d423d927b
Andrew Grant <andrew@grantnet.us>
2022-06-27 13:14:37 -0400

    Bench 3479816

--- src/search.c
+++ src/search.c
@@ -636,6 +636,9 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth) {
             // Reduce for Killers and Counters
             R -= ns->mp.stage < STAGE_QUIET;
 
+            //
+            R -= hist > 0 && ns->excluded;
+
             // Adjust based on history scores
             R -= MAX(-2, MIN(2, hist / 5000));
 
