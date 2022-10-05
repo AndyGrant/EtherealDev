18a312ce97909a69ca8977882298387c30686fba
andrew <andrew.github@grantnet.us>
2022-10-05 07:53:28 -0400

    Bench 3369281

--- src/search.c
+++ src/search.c
@@ -990,7 +990,7 @@ int singularity(Thread *thread, uint16_t ttMove, int ttValue, int depth, int PvN
     ns->excluded = NONE_MOVE;
 
     // We are going to be extending, so lets update the history for the ttmove
-    if (value < rBeta)
+    if (value < rBeta && ttValue >= beta)
         !moveIsTactical(board, ttMove)
             ? update_history_heuristics(thread, &ttMove, 1, depth)
             : update_capture_histories(thread, ttMove, &ttMove, 1, depth);
