edb06bd37cf58be54ec67c63a6faf8321986dfc2
andrew <andrew.github@grantnet.us>
2023-04-05 04:34:32 -0400

    Bench 3377645

--- src/search.c
+++ src/search.c
@@ -681,7 +681,7 @@ int search(Thread *thread, PVariation *pv, int alpha, int beta, int depth, bool 
                 R += !PvNode + !improving;
 
                 //
-                R += cutnode;
+                R += 2 * cutnode;
 
                 // Increase for King moves that evade checks
                 R += inCheck && pieceType(board->squares[MoveTo(move)]) == KING;
